b2818c8b947b68e78f0b7c2d90064fcbc8f27452
Soren Roth <sor10874@esri.com>
2023-05-10 13:04:25 -0700

    use version catalog in top level gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.0.0" apply false
-    id("com.android.library") version "8.0.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+    @Suppress("DSL_SCOPE_VIOLATION") // fixed in gradle 8.1
+    alias(libs.plugins.android.application) apply false
+    @Suppress("DSL_SCOPE_VIOLATION")
+    alias(libs.plugins.android.library) apply false
+    @Suppress("DSL_SCOPE_VIOLATION")
+    alias(libs.plugins.kotlin.android) apply false
 }
