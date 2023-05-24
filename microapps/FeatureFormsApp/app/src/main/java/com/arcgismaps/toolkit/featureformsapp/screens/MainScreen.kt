e8b267e58959298d8ab5f8a2b615e296ea44c09d
Soren Roth <sor10874@esri.com>
2023-05-24 14:52:02 -0700

    add new modules

---
+++ microapps/FeatureFormsApp/app/src/main/java/com/arcgismaps/toolkit/featureformsapp/screens/MainScreen.kt
@@ -0,0 +1,27 @@
+package com.arcgismaps.toolkit.featureformsapp.screens
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.arcgismaps.mapping.ArcGISMap
+import com.arcgismaps.mapping.BasemapStyle
+import com.arcgismaps.mapping.Viewpoint
+import com.arcgismaps.toolkit.composablemap.ComposableMap
+import com.arcgismaps.toolkit.composablemap.MapInsets
+import com.arcgismaps.toolkit.composablemap.MapData
+
+@Composable
+fun MainScreen() {
+    val map = ArcGISMap(BasemapStyle.ArcGISTopographic)
+    val mapData = MapData(
+        map = map,
+        viewPoint = Viewpoint(39.8, -98.6, 10e7),
+        insets = MapInsets(bottom = 25.0)
+    )
+    val mapViewModel = viewModel<MapViewModel>(factory = MapViewModelFactory(mapData))
+    ComposableMap(
+        modifier = Modifier.fillMaxSize(),
+        mapInterface = mapViewModel
+    )
+}
