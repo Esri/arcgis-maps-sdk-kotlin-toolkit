7abe4e482febfd4ba77f290dd09b7b3b683221ce
Kaushik Meesala <kmeesala@esri.com>
2023-05-25 15:08:42 -0700

    updated composablemaptests

--- toolkit/composable-map/src/androidTest/java/com/arcgismaps/toolkit/composablemap/ComposableMapTests.kt
+++ toolkit/composable-map/src/androidTest/java/com/arcgismaps/toolkit/composablemap/ComposableMapTests.kt
@@ -1,26 +1,21 @@
 package com.arcgismaps.toolkit.composablemap
 
-import android.view.View
 import androidx.compose.material3.Card
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.test.assert
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.hasContentDescription
-import androidx.compose.ui.test.isRoot
+import androidx.compose.ui.test.hasParent
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onRoot
-import androidx.compose.ui.test.performClick
-import androidx.compose.ui.test.printToLog
-import androidx.test.espresso.Espresso
-import com.arcgismaps.ApiKey
-import com.arcgismaps.ArcGISEnvironment
-import com.arcgismaps.BuildConfig
+import androidx.compose.ui.test.onChildAt
+import androidx.compose.ui.test.onNodeWithContentDescription
 import com.arcgismaps.mapping.ArcGISMap
 import com.arcgismaps.mapping.view.SingleTapConfirmedEvent
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import org.hamcrest.Matcher
 import org.junit.Rule
 
 import org.junit.Test
@@ -34,22 +29,27 @@ class ComposableMapTests {
     fun testComposableMapLayout() {
         val map = MapData(ArcGISMap())
         val mockMapInterface = MockMapInterface(map)
+
         composeTestRule.setContent {
             ComposableMap(mapInterface = mockMapInterface) {
-                Card(modifier = Modifier.semantics {  }) {}
+                Card(modifier = Modifier.semantics { contentDescription = "Card" }) {}
             }
         }
-        composeTestRule.onRoot().printToLog("TAG")
-        composeTestRule.onNode(hasContentDescription("MapContainer")).performClick()
-    }
+        val mapContainerSemanticLabel = "MapContainer"
+        val mapViewSemanticLabel = "MapView"
+        val contentSemanticLabel = "Content"
 
-    @Test
-    fun testComposableMapInterface() {
+        val mapContainer = composeTestRule.onNodeWithContentDescription(mapContainerSemanticLabel)
+        mapContainer.assertIsDisplayed()
+        mapContainer.onChildAt(0).assert(hasContentDescription(mapViewSemanticLabel))
+        mapContainer.onChildAt(1).assert(hasContentDescription(contentSemanticLabel))
 
+        composeTestRule.onNodeWithContentDescription("Card")
+            .assert(hasParent(hasContentDescription(contentSemanticLabel)))
     }
 }
 
-class MockMapInterface(map : MapData) : MapInterface {
+class MockMapInterface(map: MapData) : MapInterface {
     private val _mapData = MutableStateFlow(map)
     override val mapData: StateFlow<MapData> = _mapData.asStateFlow()
 
