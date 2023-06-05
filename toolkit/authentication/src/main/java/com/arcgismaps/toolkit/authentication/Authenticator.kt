23158cce11dc2da245df8bca93236de8bf56d71b
Hudson Miears <hud10837@esri.com>
2023-06-05 17:58:17 +0300

    rename views and move OAuthAuthenticator to own file

--- toolkit/authentication/src/main/java/com/arcgismaps/toolkit/authentication/Authenticator.kt
+++ toolkit/authentication/src/main/java/com/arcgismaps/toolkit/authentication/Authenticator.kt
@@ -1,12 +1,9 @@
 package com.arcgismaps.toolkit.authentication
 
-import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.collectAsState
 import com.arcgismaps.ArcGISEnvironment
 import com.arcgismaps.httpcore.authentication.OAuthUserConfiguration
-import com.arcgismaps.httpcore.authentication.OAuthUserSignIn
 
 /**
  * Displays appropriate Authentication UI when issued a challenge. For example, if an OAuth challenge
@@ -38,30 +35,5 @@ public fun Authenticator(
         OAuthAuthenticator(oAuthPendingSignIn) { redirectUrl ->
             authenticatorViewModel.oAuthUserSignInManager.completeOAuthPendingSignIn(redirectUrl = redirectUrl)
         }
-    }
-}
-
-/**
- * Launches a Custom Chrome Tab using the url in [oAuthPendingSignIn] and calls [onActivityResult] on completion.
- *
- * @see OAuthUserSignInActivity
- * @param oAuthPendingSignIn the [OAuthUserSignIn] pending completion.
- * @param onActivityResult called with the redirect url on completion of the OAuth sign in.
- * @since 200.2.0
- */
-@Composable
-private fun OAuthAuthenticator(
-    oAuthPendingSignIn: OAuthUserSignIn,
-    onActivityResult: (String?) -> Unit
-) {
-    val launcher =
-        rememberLauncherForActivityResult(contract = OAuthUserSignInActivity.Contract()) { redirectUrl ->
-            onActivityResult(redirectUrl)
-        }
-    // Launching an activity is a side effect. We don't need `LaunchedEffect` because this is not suspending
-    // and there's nothing that needs to keep running if it gets recomposed. In reality, we also don't
-    // expect `oAuthPendingSignIn` to change while this composable is displayed.
-    SideEffect {
-        launcher.launch(oAuthPendingSignIn)
     }
 }
