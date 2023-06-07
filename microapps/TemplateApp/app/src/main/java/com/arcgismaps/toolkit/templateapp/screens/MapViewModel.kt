d2d04808bc833a9f2fdc1fde8695c334fb11f5a4
Soren Roth <sor10874@esri.com>
2023-06-07 10:28:11 -0700

    newline. example of a touch handler that has a MapView and coroutinescope in context

--- microapps/TemplateApp/app/src/main/java/com/arcgismaps/toolkit/templateapp/screens/MapViewModel.kt
+++ microapps/TemplateApp/app/src/main/java/com/arcgismaps/toolkit/templateapp/screens/MapViewModel.kt
@@ -4,8 +4,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.arcgismaps.mapping.ArcGISMap
 import com.arcgismaps.mapping.Viewpoint
+import com.arcgismaps.mapping.view.MapView
+import com.arcgismaps.mapping.view.SingleTapConfirmedEvent
 import com.arcgismaps.toolkit.composablemap.MapInsets
 import com.arcgismaps.toolkit.composablemap.MapInterface
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -23,6 +26,12 @@ class MapViewModel(
 
     private val _currentViewpoint: MutableStateFlow<Viewpoint?> = MutableStateFlow(null)
     override val currentViewpoint: StateFlow<Viewpoint?> = _currentViewpoint.asStateFlow()
+    
+    context(MapView, CoroutineScope) override fun onSingleTapConfirmed(singleTapEvent: SingleTapConfirmedEvent) {
+        // example of how to add a tap handler with a MapView and CoroutineScope in context.
+        // the MapView and the rememberer CoroutineScope are available as context objects and may be referenced
+        // using `this@MapView`, or without a `this` handle at all. Do not leak these references.
+    }
 
     fun setViewpoint(viewpoint: Viewpoint) {
         _currentViewpoint.update { viewpoint }
