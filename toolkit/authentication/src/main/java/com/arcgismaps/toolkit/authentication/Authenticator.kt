bfa5803064fbe41de04c6b5b04235bfb43794dd2
Hudson Miears <hud10837@esri.com>
2023-06-08 17:16:11 +0300

    use collectAsStateWithLifecycle

--- toolkit/authentication/src/main/java/com/arcgismaps/toolkit/authentication/Authenticator.kt
+++ toolkit/authentication/src/main/java/com/arcgismaps/toolkit/authentication/Authenticator.kt
@@ -2,10 +2,12 @@ package com.arcgismaps.toolkit.authentication
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.arcgismaps.httpcore.authentication.ArcGISAuthenticationChallenge
 import com.arcgismaps.httpcore.authentication.OAuthUserConfiguration
 
 /**
- * Displays appropriate Authentication UI when issued a challenge. For example, if an OAuth challenge
+ * Displays appropriate Authentication UI when issued a challenge. For example, if an [ArcGISAuthenticationChallenge]
  * is issued and the [AuthenticatorViewModel.oAuthUserSignInManager] has a corresponding [OAuthUserConfiguration],
  * then a Custom Chrome Tab will be launched to complete the OAuth sign in.
  *
@@ -17,7 +19,7 @@ public fun Authenticator(
     authenticatorViewModel: AuthenticatorViewModel
 ) {
     val oAuthPendingSignIn =
-        authenticatorViewModel.oAuthUserSignInManager.pendingOAuthUserSignIn.collectAsState().value
+        authenticatorViewModel.oAuthUserSignInManager.pendingOAuthUserSignIn.collectAsStateWithLifecycle().value
 
     oAuthPendingSignIn?.let { oAuthPendingSignIn ->
         OAuthAuthenticator(oAuthPendingSignIn) { redirectUrl ->
