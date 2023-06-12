d0c56ba4d2f8d19ceb39d1c02372a49387e5fec4
Soren Roth <sor10874@esri.com>
2023-06-12 10:30:06 -0700

    add TopBVar preview, use theme, string resource.

--- microapps/FeatureFormsApp/app/src/main/java/com/arcgismaps/toolkit/featureformsapp/screens/mapview/MapScreen.kt
+++ microapps/FeatureFormsApp/app/src/main/java/com/arcgismaps/toolkit/featureformsapp/screens/mapview/MapScreen.kt
@@ -8,9 +8,11 @@ import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.material3.rememberStandardBottomSheetState
 import androidx.compose.runtime.Composable
@@ -19,6 +21,8 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.arcgismaps.mapping.ArcGISMap
@@ -27,6 +31,7 @@ import com.arcgismaps.toolkit.composablemap.MapInsets
 import com.arcgismaps.toolkit.featureforms.FeatureForm
 import com.arcgismaps.toolkit.featureforms.FeatureFormViewModelFactory
 import com.arcgismaps.toolkit.featureforms.FeatureFormViewModelImpl
+import com.arcgismaps.toolkit.featureformsapp.R
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -76,7 +81,18 @@ fun MapScreen() {
         topBar = if (sheetVisibility.value) {
             {
                 TopAppBar(
-                    title = { Text(text = "Edit Feature") },
+                    title = { Text(text = stringResource(R.string.edit_feature), color = Color.White) },
+                    navigationIcon = {
+                        IconButton(onClick = {
+                            formViewModel.setFormVisibility(false)
+                        }) {
+                            Icon(
+                                imageVector = Icons.Default.Close,
+                                contentDescription = "Close Feature Editor",
+                                tint = Color.White
+                            )
+                        }
+                    },
                     actions = {
                         IconButton(onClick = {
                             /* FUTURE: save feature here */
@@ -85,19 +101,14 @@ fun MapScreen() {
                             Icon(
                                 imageVector = Icons.Default.Check,
                                 contentDescription = "Save Feature",
-                                tint = Color.Black
+                                tint = Color.White
                             )
                         }
-                        IconButton(onClick = {
-                            formViewModel.setFormVisibility(false)
-                        }) {
-                            Icon(
-                                imageVector = Icons.Default.Close,
-                                contentDescription = "Close Feature Editor",
-                                tint = Color.Black
-                            )
-                        }
-                    }
+                        
+                    },
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primary
+                    )
                 )
             }
         } else {
@@ -111,3 +122,34 @@ fun MapScreen() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun TopFormBarPreview() {
+    TopAppBar(
+        title = { Text(text = "Edit Feature", color = Color.White) },
+        navigationIcon = {
+            IconButton(onClick = {
+            }) {
+                Icon(
+                    imageVector = Icons.Default.Close,
+                    contentDescription = "Close Feature Editor",
+                    tint = Color.White
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = {
+            }) {
+                Icon(
+                    imageVector = Icons.Default.Check,
+                    contentDescription = "Save Feature",
+                    tint = Color.White
+                )
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        )
+    )
+}
