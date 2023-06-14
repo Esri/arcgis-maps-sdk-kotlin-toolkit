78c785ac84e96c54eed3b27ed6868367f8e30391
Soren Roth <sor10874@esri.com>
2023-06-14 13:32:43 -0700

    remove mapInsets parameter and unnecessary overrides

--- microapps/FeatureFormsApp/app/src/main/java/com/arcgismaps/toolkit/featureformsapp/screens/mapview/FeatureFormsMapViewModel.kt
+++ microapps/FeatureFormsApp/app/src/main/java/com/arcgismaps/toolkit/featureformsapp/screens/mapview/FeatureFormsMapViewModel.kt
@@ -4,17 +4,12 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.arcgismaps.data.ArcGISFeature
 import com.arcgismaps.mapping.ArcGISMap
-import com.arcgismaps.mapping.Viewpoint
 import com.arcgismaps.mapping.view.IdentifyLayerResult
 import com.arcgismaps.mapping.view.MapView
 import com.arcgismaps.mapping.view.SingleTapConfirmedEvent
-import com.arcgismaps.toolkit.composablemap.MapInsets
 import com.arcgismaps.toolkit.composablemap.MapInterface
 import com.arcgismaps.toolkit.composablemap.MapInterfaceImpl
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 /**
@@ -25,18 +20,8 @@ import kotlinx.coroutines.launch
  */
 class FeatureFormsMapViewModelImpl(
     arcGISMap: ArcGISMap,
-    mapInsets: MapInsets = MapInsets(),
     val onFeatureIdentified: (ArcGISFeature) -> Unit
-) : ViewModel(), MapInterface by MapInterfaceImpl(arcGISMap, mapInsets) {
-    private val _map: MutableStateFlow<ArcGISMap> = MutableStateFlow(arcGISMap)
-    override val map: StateFlow<ArcGISMap> = _map.asStateFlow()
-    
-    private val _insets: MutableStateFlow<MapInsets> = MutableStateFlow(mapInsets)
-    override val insets: StateFlow<MapInsets> = _insets.asStateFlow()
-    
-    private val _currentViewpoint: MutableStateFlow<Viewpoint?> = MutableStateFlow(null)
-    override val currentViewpoint: StateFlow<Viewpoint?> = _currentViewpoint.asStateFlow()
-    
+) : ViewModel(), MapInterface by MapInterfaceImpl(arcGISMap) {
     private suspend fun onIdentifyLayers(results: List<IdentifyLayerResult>) {
         val popup = results.firstOrNull { result ->
             result.popups.isNotEmpty()
@@ -62,11 +47,10 @@ class FeatureFormsMapViewModelImpl(
 
 class FeatureFormsMapViewModelFactory(
     private val arcGISMap: ArcGISMap,
-    private val mapInsets: MapInsets = MapInsets(),
     private val onFeatureIdentified: (ArcGISFeature) -> Unit = {}
 ) : ViewModelProvider.NewInstanceFactory() {
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return FeatureFormsMapViewModelImpl(arcGISMap, mapInsets, onFeatureIdentified) as T
+        return FeatureFormsMapViewModelImpl(arcGISMap, onFeatureIdentified) as T
     }
 }
