64cfe32fbab5e651a2d868af6e6237460340ab69
Kaushik Meesala <kmeesala@esri.com>
2023-06-29 16:21:18 -0700

    Update FeatureForm.kt

--- toolkit/featureforms/src/main/java/com/arcgismaps/toolkit/featureforms/FeatureForm.kt
+++ toolkit/featureforms/src/main/java/com/arcgismaps/toolkit/featureforms/FeatureForm.kt
@@ -1,6 +1,5 @@
 package com.arcgismaps.toolkit.featureforms
 
-import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
