000bca9017ad26ec6faa5fd4b40caac8b4d8ffee
Hudson Miears <hud10837@esri.com>
2023-06-29 10:05:59 +0300

    add support for pressing tab and enter

--- toolkit/authentication/src/main/java/com/arcgismaps/toolkit/authentication/UsernamePasswordAuthenticator.kt
+++ toolkit/authentication/src/main/java/com/arcgismaps/toolkit/authentication/UsernamePasswordAuthenticator.kt
@@ -22,7 +22,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyEventType
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onPreviewKeyEvent
+import androidx.compose.ui.input.key.type
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -56,7 +65,12 @@ public fun UsernamePasswordAuthenticator(
             text = buildAnnotatedString {
                 append(stringResource(id = R.string.username_password_login_message))
                 append(" ")
-                withStyle(style = SpanStyle(fontFamily = FontFamily.Monospace, fontSize = MaterialTheme.typography.headlineSmall.fontSize)) {
+                withStyle(
+                    style = SpanStyle(
+                        fontFamily = FontFamily.Monospace,
+                        fontSize = MaterialTheme.typography.headlineSmall.fontSize
+                    )
+                ) {
                     append(usernamePasswordChallenge.url)
                 }
             },
@@ -81,7 +95,14 @@ public fun UsernamePasswordAuthenticator(
                     }
                 )
             }
+            val focusManager = LocalFocusManager.current
             OutlinedTextField(
+                modifier = Modifier.moveFocusOnTabEvent(focusManager, {
+                    usernamePasswordChallenge.continueWithCredentials(
+                        usernameFieldText, passwordFieldText
+                    )
+                }
+                ),
                 value = usernameFieldText,
                 onValueChange = { it: String -> usernameFieldText = it },
                 keyboardOptions = KeyboardOptions(
@@ -93,6 +114,12 @@ public fun UsernamePasswordAuthenticator(
                 singleLine = true
             )
             OutlinedTextField(
+                modifier = Modifier.moveFocusOnTabEvent(focusManager, {
+                    usernamePasswordChallenge.continueWithCredentials(
+                        usernameFieldText, passwordFieldText
+                    )
+                }
+                ),
                 value = passwordFieldText,
                 onValueChange = { it: String -> passwordFieldText = it },
                 keyboardOptions = KeyboardOptions(
@@ -121,7 +148,24 @@ public fun UsernamePasswordAuthenticator(
                     Text(stringResource(id = R.string.login))
                 }
             }
-
         }
     }
 }
+
+@OptIn(ExperimentalComposeUiApi::class)
+private fun Modifier.moveFocusOnTabEvent(focusManager: FocusManager, onEnter: () -> Unit) =
+    onPreviewKeyEvent {
+        if (it.type == KeyEventType.KeyDown) {
+            when (it.key.keyCode) {
+                Key.Tab.keyCode -> {
+                    focusManager.moveFocus(FocusDirection.Down); true
+                }
+
+                Key.Enter.keyCode -> {
+                    onEnter(); true
+                }
+
+                else -> false
+            }
+        } else false
+    }
