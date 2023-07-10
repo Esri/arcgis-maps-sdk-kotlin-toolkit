5e6fe240a137e5d77a262fa641b506ca33be6c65
Hudson Miears <hud10837@esri.com>
2023-07-10 09:57:28 +0300

    move things around

--- toolkit/authentication/src/main/java/com/arcgismaps/toolkit/authentication/OAuthAuthenticator.kt
+++ toolkit/authentication/src/main/java/com/arcgismaps/toolkit/authentication/OAuthAuthenticator.kt
@@ -21,16 +21,16 @@ import com.arcgismaps.httpcore.authentication.OAuthUserSignIn
 internal fun OAuthAuthenticator(
     oAuthPendingSignIn: OAuthUserSignIn
 ) {
-    // If a configuration change happens while the OAuth activity is active, then when the activity
-    // returns, this composable would launch the activity again due to the `SideEffect` below. This
-    // flag prevents a relaunch.
-    var didLaunch by rememberSaveable{ mutableStateOf(false) }
     val launcher =
         rememberLauncherForActivityResult(contract = OAuthUserSignInActivity.Contract()) { redirectUrl ->
             redirectUrl?.let {
                 oAuthPendingSignIn.complete(redirectUrl)
             } ?: oAuthPendingSignIn.cancel()
         }
+    // If a configuration change happens while the OAuth activity is active, then when the activity
+    // returns, this composable would launch the activity again due to the `SideEffect` below. This
+    // flag prevents a relaunch.
+    var didLaunch by rememberSaveable{ mutableStateOf(false) }
     // Launching an activity is a side effect. We don't need `LaunchedEffect` because this is not suspending
     // and there's nothing that needs to keep running if it gets recomposed. In reality, we also don't
     // expect `oAuthPendingSignIn` to change while this composable is displayed.
