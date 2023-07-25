cb340a2729556197d3504a4089082bc50d47e6f9
Hudson Miears <hud10837@esri.com>
2023-07-25 17:27:44 +0300

    Refactor OAuthUserSignInActivity to allow for user activities to intercept the CCT redirect

--- toolkit/authentication/src/main/java/com/arcgismaps/toolkit/authentication/OAuthUserSignInActivity.kt
+++ toolkit/authentication/src/main/java/com/arcgismaps/toolkit/authentication/OAuthUserSignInActivity.kt
@@ -26,14 +26,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.browser.customtabs.CustomTabsIntent
-import androidx.lifecycle.Lifecycle
 import com.arcgismaps.httpcore.authentication.OAuthUserSignIn
 
 private const val KEY_INTENT_EXTRA_AUTHORIZE_URL = "INTENT_EXTRA_KEY_AUTHORIZE_URL"
-private const val KEY_INTENT_EXTRA_CUSTOM_TABS_WAS_LAUNCHED =
-    "KEY_INTENT_EXTRA_CUSTOM_TABS_WAS_LAUNCHED"
 private const val KEY_INTENT_EXTRA_OAUTH_RESPONSE_URL = "KEY_INTENT_EXTRA_OAUTH_RESPONSE_URI"
-private const val KEY_INTENT_EXTRA_REDIRECT_URL = "KEY_INTENT_EXTRA_REDIRECT_URL"
+private const val KEY_INTENT_EXTRA_PROMPT_SIGN_IN = "KEY_INTENT_EXTRA_PROMPT_SIGN_IN"
 
 private const val RESULT_CODE_SUCCESS = 1
 private const val RESULT_CODE_CANCELED = 2
@@ -44,66 +41,37 @@ private const val RESULT_CODE_CANCELED = 2
  *
  * @since 200.2.0
  */
-internal class OAuthUserSignInActivity : ComponentActivity() {
-
-    private var customTabsWasLaunched = false
-    private lateinit var redirectUrl: String
+public class OAuthUserSignInActivity : ComponentActivity() {
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // redirect URL should be a valid string since we are adding it in the ActivityResultContract
-        redirectUrl = intent.getStringExtra(KEY_INTENT_EXTRA_REDIRECT_URL).toString()
-
-        customTabsWasLaunched =
-            savedInstanceState?.getBoolean(KEY_INTENT_EXTRA_CUSTOM_TABS_WAS_LAUNCHED) ?: false
-
-        if (!customTabsWasLaunched) {
+        if (intent.hasExtra(KEY_INTENT_EXTRA_PROMPT_SIGN_IN)) {
             val authorizeUrl = intent.getStringExtra(KEY_INTENT_EXTRA_AUTHORIZE_URL)
             authorizeUrl?.let {
                 launchCustomTabs(it)
             }
         }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-
-        outState.putBoolean(KEY_INTENT_EXTRA_CUSTOM_TABS_WAS_LAUNCHED, customTabsWasLaunched)
+        else {
+            handleRedirectIntent(intent)
+        }
     }
 
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
-
-        intent?.data?.let { uri ->
-            val uriString = uri.toString()
-            if (uriString.startsWith(redirectUrl)) {
-                val newIntent = Intent().apply {
-                    putExtra(KEY_INTENT_EXTRA_OAUTH_RESPONSE_URL, uri.toString())
-                }
-                setResult(RESULT_CODE_SUCCESS, newIntent)
-            } else {
-                // the uri likely contains an error, for example if the user hits the cancel button
-                // on the custom tab prompt
-                setResult(RESULT_CODE_CANCELED, Intent())
-            }
-            finish()
-        }
+        handleRedirectIntent(intent)
     }
 
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-
-        // We only want to respond to focus changed events when this activity is in "resumed" state.
-        // On some devices (Oreo) we get unexpected focus changed events with hasFocus true which cause this Activity
-        // to be finished (destroyed) prematurely, for example:
-        // - On Oreo log in to portal with OAuth
-        // - When the browser window is launched this triggers a focus changed event with hasFocus true but at this point
-        //   we do not want to finish this activity -> at this point the activity is in paused state (isResumed == false) so
-        //   we can use this to ignore this "rogue" focus changed event.
-        if (hasFocus && lifecycle.currentState.isAtLeast(Lifecycle.State.RESUMED)) {
-            // if we got here the user must have pressed the back button or the x button while the
-            // custom tab was visible - finish by cancelling OAuth sign in
-            setResult(RESULT_CODE_CANCELED, Intent())
+    public fun handleRedirectIntent(intent: Intent?) {
+        intent?.data?.let { uri ->
+            val uriString = uri.toString()
+            val newIntent = Intent().apply {
+                putExtra(KEY_INTENT_EXTRA_OAUTH_RESPONSE_URL, uriString)
+            }
+            setResult(RESULT_CODE_SUCCESS, newIntent)
+            finish()
+        } ?: {
+            setResult(RESULT_CODE_CANCELED)
             finish()
         }
     }
@@ -117,7 +85,6 @@ internal class OAuthUserSignInActivity : ComponentActivity() {
      * @since 200.2.0
      */
     private fun launchCustomTabs(authorizeUrl: String) {
-        customTabsWasLaunched = true
         CustomTabsIntent.Builder().build().launchUrl(this, Uri.parse(authorizeUrl))
     }
 
@@ -131,14 +98,11 @@ internal class OAuthUserSignInActivity : ComponentActivity() {
      *
      * @since 200.2.0
      */
-    class Contract : ActivityResultContract<OAuthUserSignIn, String?>() {
+    public class Contract : ActivityResultContract<OAuthUserSignIn, String?>() {
         override fun createIntent(context: Context, input: OAuthUserSignIn): Intent =
             Intent(context, OAuthUserSignInActivity::class.java).apply {
                 putExtra(KEY_INTENT_EXTRA_AUTHORIZE_URL, input.authorizeUrl)
-                putExtra(
-                    KEY_INTENT_EXTRA_REDIRECT_URL,
-                    input.oAuthUserConfiguration.redirectUrl
-                )
+                putExtra(KEY_INTENT_EXTRA_PROMPT_SIGN_IN, true)
             }
 
         override fun parseResult(resultCode: Int, intent: Intent?): String? {
