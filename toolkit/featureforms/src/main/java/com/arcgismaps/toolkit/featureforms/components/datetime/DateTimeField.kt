b2fb037c73130700296b4244f342383ebf13d0de
Soren Roth <sor10874@esri.com>
2023-07-31 17:18:47 -0700

    Use PlaceHolder for the hint.

--- toolkit/featureforms/src/main/java/com/arcgismaps/toolkit/featureforms/components/datetime/DateTimeField.kt
+++ toolkit/featureforms/src/main/java/com/arcgismaps/toolkit/featureforms/components/datetime/DateTimeField.kt
@@ -45,12 +45,14 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.arcgismaps.toolkit.featureforms.R
 import com.arcgismaps.toolkit.featureforms.components.datetime.picker.DateTimePicker
 import com.arcgismaps.toolkit.featureforms.components.datetime.picker.DateTimePickerState
 import com.arcgismaps.toolkit.featureforms.components.datetime.picker.DateTimePickerStyle
+import com.arcgismaps.toolkit.featureforms.utils.PlaceholderTransformation
 
 @Composable
 internal fun DateTimeField(
@@ -143,7 +145,7 @@ internal fun DateTimeField(
                 .padding(start = 15.dp, end = 15.dp, top = 10.dp, bottom = 10.dp)
         ) {
             OutlinedTextField(
-                value = epochMillis?.formattedDateTime() ?: stringResource(id = R.string.novalue),
+                value = epochMillis?.formattedDateTime() ?: "",
                 onValueChange = {},
                 modifier = Modifier
                     .fillMaxSize()
@@ -195,13 +197,16 @@ internal fun DateTimeField(
                         Text(text = state.description)
                     }
                 },
+                visualTransformation = if (state.value.value == null)
+                    PlaceholderTransformation(stringResource(id = R.string.novalue))
+                else VisualTransformation.None,
                 singleLine = true,
                 colors = textFieldColors
             )
         }
     } else {
         ImmutableDate(
-            valueString = epochMillis?.formattedDateTime() ?: stringResource(id = R.string.novalue),
+            valueString = epochMillis?.formattedDateTime() ?: "",
             label = state.label,
             supportingText = state.description
         )
@@ -235,6 +240,9 @@ private fun ImmutableDate(
             readOnly = true,
             label = { Text(text = label) },
             supportingText = { Text(text = supportingText) },
+            visualTransformation = if (valueString.isEmpty())
+                PlaceholderTransformation(stringResource(id = R.string.novalue))
+            else VisualTransformation.None,
             colors = colors
         )
     }
