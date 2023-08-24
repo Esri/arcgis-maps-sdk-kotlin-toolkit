973277ee9a77aa718c7366faee7ef982dfbbb085
Soren Roth <sor10874@esri.com>
2023-08-24 12:29:28 -0700

    remove logging

--- toolkit/featureforms/src/main/java/com/arcgismaps/toolkit/featureforms/components/datetime/picker/DateTimePickerState.kt
+++ toolkit/featureforms/src/main/java/com/arcgismaps/toolkit/featureforms/components/datetime/picker/DateTimePickerState.kt
@@ -188,10 +188,8 @@ private class DateTimePickerStateImpl(
 
     override fun togglePickerInput() {
         activePickerInput.value = if (activePickerInput.value == DateTimePickerInput.Date) {
-            println("toggling to time")
             DateTimePickerInput.Time
         } else {
-            println("toggling to date")
             DateTimePickerInput.Date
         }
     }
