43e9d03fce3f88ccb3b32f9035fd9a97f9d8a9b7
Kaushik Meesala <kmeesala@esri.com>
2023-09-06 14:16:09 -0700

    init copy

---
+++ toolkit/featureforms/src/main/java/com/arcgismaps/toolkit/featureforms/components/datetime/picker/time/Strings.kt
@@ -0,0 +1,54 @@
+package com.arcgismaps.toolkit.featureforms.components.datetime.picker.time
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.material3.R
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
+
+@Immutable
+@JvmInline
+internal value class Strings private constructor(
+    @Suppress("unused") private val value: Int = nextId()
+) {
+    companion object {
+        private var id = 0
+        private fun nextId() = id++
+
+        val TimePickerAM = Strings()
+        val TimePickerPM = Strings()
+        val TimePickerPeriodToggle = Strings()
+        val TimePickerHourSelection = Strings()
+        val TimePickerMinuteSelection = Strings()
+        val TimePickerHourSuffix = Strings()
+        val TimePicker24HourSuffix = Strings()
+        val TimePickerMinuteSuffix = Strings()
+        val TimePickerHour = Strings()
+        val TimePickerMinute = Strings()
+        val TimePickerHourTextField = Strings()
+        val TimePickerMinuteTextField = Strings()
+    }
+}
+
+@Composable
+@ReadOnlyComposable
+internal fun getString(string: Strings): String {
+    LocalConfiguration.current
+    val resources = LocalContext.current.resources
+    return when (string) {
+        Strings.TimePickerAM -> resources.getString(R.string.time_picker_am)
+        Strings.TimePickerPM -> resources.getString(R.string.time_picker_pm)
+        Strings.TimePickerPeriodToggle -> resources.getString(R.string.time_picker_period_toggle_description)
+        Strings.TimePickerMinuteSelection -> resources.getString(R.string.time_picker_minute_selection)
+        Strings.TimePickerHourSelection -> resources.getString(R.string.time_picker_hour_selection)
+        Strings.TimePickerHourSuffix -> resources.getString(R.string.time_picker_hour_suffix)
+        Strings.TimePickerMinuteSuffix -> resources.getString(R.string.time_picker_minute_suffix)
+        Strings.TimePicker24HourSuffix -> resources.getString(R.string.time_picker_hour_24h_suffix)
+        Strings.TimePickerHour -> resources.getString(R.string.time_picker_hour)
+        Strings.TimePickerMinute -> resources.getString(R.string.time_picker_minute)
+        Strings.TimePickerHourTextField -> resources.getString(R.string.time_picker_hour_text_field)
+        Strings.TimePickerMinuteTextField -> resources.getString(R.string.time_picker_minute_text_field)
+        else -> ""
+    }
+}
---
+++ toolkit/featureforms/src/main/java/com/arcgismaps/toolkit/featureforms/components/datetime/picker/time/TimePickerTokens.kt
@@ -0,0 +1,104 @@
+package com.arcgismaps.toolkit.featureforms.components.datetime.picker.time
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.tokens.ColorSchemeKeyTokens
+import androidx.compose.material3.tokens.ElevationTokens
+import androidx.compose.material3.tokens.ShapeKeyTokens
+import androidx.compose.material3.tokens.TypographyKeyTokens
+import androidx.compose.ui.unit.dp
+
+internal object TimePickerTokens {
+    val ClockDialColor = MaterialTheme.colorScheme.SurfaceVariant
+    val ClockDialContainerSize = 256.0.dp
+    val ClockDialLabelTextFont = TypographyKeyTokens.BodyLarge
+    val ClockDialSelectedLabelTextColor = ColorSchemeKeyTokens.OnPrimary
+    val ClockDialSelectorCenterContainerColor = ColorSchemeKeyTokens.Primary
+    val ClockDialSelectorCenterContainerShape = ShapeKeyTokens.CornerFull
+    val ClockDialSelectorCenterContainerSize = 8.0.dp
+    val ClockDialSelectorHandleContainerColor = ColorSchemeKeyTokens.Primary
+    val ClockDialSelectorHandleContainerShape = ShapeKeyTokens.CornerFull
+    val ClockDialSelectorHandleContainerSize = 48.0.dp
+    val ClockDialSelectorTrackContainerColor = ColorSchemeKeyTokens.Primary
+    val ClockDialSelectorTrackContainerWidth = 2.0.dp
+    val ClockDialShape = ShapeKeyTokens.CornerFull
+    val ClockDialUnselectedLabelTextColor = ColorSchemeKeyTokens.OnSurface
+    val ContainerColor = ColorSchemeKeyTokens.Surface
+    val ContainerElevation = ElevationTokens.Level3
+    val ContainerShape = ShapeKeyTokens.CornerExtraLarge
+    val HeadlineColor = ColorSchemeKeyTokens.OnSurfaceVariant
+    val HeadlineFont = TypographyKeyTokens.LabelMedium
+    val PeriodSelectorContainerShape = ShapeKeyTokens.CornerSmall
+    val PeriodSelectorHorizontalContainerHeight = 38.0.dp
+    val PeriodSelectorHorizontalContainerWidth = 216.0.dp
+    val PeriodSelectorLabelTextFont = TypographyKeyTokens.TitleMedium
+    val PeriodSelectorOutlineColor = ColorSchemeKeyTokens.Outline
+    val PeriodSelectorOutlineWidth = 1.0.dp
+    val PeriodSelectorSelectedContainerColor = ColorSchemeKeyTokens.TertiaryContainer
+    val PeriodSelectorSelectedFocusLabelTextColor = ColorSchemeKeyTokens.OnTertiaryContainer
+    val PeriodSelectorSelectedHoverLabelTextColor = ColorSchemeKeyTokens.OnTertiaryContainer
+    val PeriodSelectorSelectedLabelTextColor = ColorSchemeKeyTokens.OnTertiaryContainer
+    val PeriodSelectorSelectedPressedLabelTextColor = ColorSchemeKeyTokens.OnTertiaryContainer
+    val PeriodSelectorUnselectedFocusLabelTextColor = ColorSchemeKeyTokens.OnSurfaceVariant
+    val PeriodSelectorUnselectedHoverLabelTextColor = ColorSchemeKeyTokens.OnSurfaceVariant
+    val PeriodSelectorUnselectedLabelTextColor = ColorSchemeKeyTokens.OnSurfaceVariant
+    val PeriodSelectorUnselectedPressedLabelTextColor = ColorSchemeKeyTokens.OnSurfaceVariant
+    val PeriodSelectorVerticalContainerHeight = 80.0.dp
+    val PeriodSelectorVerticalContainerWidth = 52.0.dp
+    val SurfaceTintLayerColor = ColorSchemeKeyTokens.SurfaceTint
+    val TimeSelector24HVerticalContainerWidth = 114.0.dp
+    val TimeSelectorContainerHeight = 80.0.dp
+    val TimeSelectorContainerShape = ShapeKeyTokens.CornerSmall
+    val TimeSelectorContainerWidth = 96.0.dp
+    val TimeSelectorLabelTextFont = TypographyKeyTokens.DisplayLarge
+    val TimeSelectorSelectedContainerColor = ColorSchemeKeyTokens.PrimaryContainer
+    val TimeSelectorSelectedFocusLabelTextColor = ColorSchemeKeyTokens.OnPrimaryContainer
+    val TimeSelectorSelectedHoverLabelTextColor = ColorSchemeKeyTokens.OnPrimaryContainer
+    val TimeSelectorSelectedLabelTextColor = ColorSchemeKeyTokens.OnPrimaryContainer
+    val TimeSelectorSelectedPressedLabelTextColor = ColorSchemeKeyTokens.OnPrimaryContainer
+    val TimeSelectorSeparatorColor = ColorSchemeKeyTokens.OnSurface
+    val TimeSelectorSeparatorFont = TypographyKeyTokens.DisplayLarge
+    val TimeSelectorUnselectedContainerColor = ColorSchemeKeyTokens.SurfaceVariant
+    val TimeSelectorUnselectedFocusLabelTextColor = ColorSchemeKeyTokens.OnSurface
+    val TimeSelectorUnselectedHoverLabelTextColor = ColorSchemeKeyTokens.OnSurface
+    val TimeSelectorUnselectedLabelTextColor = ColorSchemeKeyTokens.OnSurface
+    val TimeSelectorUnselectedPressedLabelTextColor = ColorSchemeKeyTokens.OnSurface
+}
+
+internal object TimeInputTokens {
+    val ContainerColor = ColorSchemeKeyTokens.Surface
+    val ContainerElevation = ElevationTokens.Level3
+    val ContainerShape = ShapeKeyTokens.CornerExtraLarge
+    val HeadlineColor = ColorSchemeKeyTokens.OnSurfaceVariant
+    val HeadlineFont = TypographyKeyTokens.LabelMedium
+    val PeriodSelectorContainerHeight = 72.0.dp
+    val PeriodSelectorContainerShape = ShapeKeyTokens.CornerSmall
+    val PeriodSelectorContainerWidth = 52.0.dp
+    val PeriodSelectorLabelTextFont = TypographyKeyTokens.TitleMedium
+    val PeriodSelectorOutlineColor = ColorSchemeKeyTokens.Outline
+    val PeriodSelectorOutlineWidth = 1.0.dp
+    val PeriodSelectorSelectedContainerColor = ColorSchemeKeyTokens.TertiaryContainer
+    val PeriodSelectorSelectedFocusLabelTextColor = ColorSchemeKeyTokens.OnTertiaryContainer
+    val PeriodSelectorSelectedHoverLabelTextColor = ColorSchemeKeyTokens.OnTertiaryContainer
+    val PeriodSelectorSelectedLabelTextColor = ColorSchemeKeyTokens.OnTertiaryContainer
+    val PeriodSelectorSelectedPressedLabelTextColor = ColorSchemeKeyTokens.OnTertiaryContainer
+    val PeriodSelectorUnselectedFocusLabelTextColor = ColorSchemeKeyTokens.OnSurfaceVariant
+    val PeriodSelectorUnselectedHoverLabelTextColor = ColorSchemeKeyTokens.OnSurfaceVariant
+    val PeriodSelectorUnselectedLabelTextColor = ColorSchemeKeyTokens.OnSurfaceVariant
+    val PeriodSelectorUnselectedPressedLabelTextColor = ColorSchemeKeyTokens.OnSurfaceVariant
+    val SurfaceTintLayerColor = ColorSchemeKeyTokens.SurfaceTint
+    val TimeFieldContainerColor = ColorSchemeKeyTokens.SurfaceVariant
+    val TimeFieldContainerHeight = 72.0.dp
+    val TimeFieldContainerShape = ShapeKeyTokens.CornerSmall
+    val TimeFieldContainerWidth = 96.0.dp
+    val TimeFieldFocusContainerColor = ColorSchemeKeyTokens.PrimaryContainer
+    val TimeFieldFocusLabelTextColor = ColorSchemeKeyTokens.OnPrimaryContainer
+    val TimeFieldFocusOutlineColor = ColorSchemeKeyTokens.Primary
+    val TimeFieldFocusOutlineWidth = 2.0.dp
+    val TimeFieldHoverLabelTextColor = ColorSchemeKeyTokens.OnSurface
+    val TimeFieldLabelTextColor = ColorSchemeKeyTokens.OnSurface
+    val TimeFieldLabelTextFont = TypographyKeyTokens.DisplayMedium
+    val TimeFieldSeparatorColor = ColorSchemeKeyTokens.OnSurface
+    val TimeFieldSeparatorFont = TypographyKeyTokens.DisplayLarge
+    val TimeFieldSupportingTextColor = ColorSchemeKeyTokens.OnSurfaceVariant
+    val TimeFieldSupportingTextFont = TypographyKeyTokens.BodySmall
+}
