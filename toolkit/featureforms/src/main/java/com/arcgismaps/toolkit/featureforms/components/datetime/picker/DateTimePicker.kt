44600e8cbaf10b85bb5a7b339a59ca0b2d180c75
Soren Roth <sor10874@esri.com>
2023-09-07 09:53:36 -0700

    remove commented out code

--- toolkit/featureforms/src/main/java/com/arcgismaps/toolkit/featureforms/components/datetime/picker/DateTimePicker.kt
+++ toolkit/featureforms/src/main/java/com/arcgismaps/toolkit/featureforms/components/datetime/picker/DateTimePicker.kt
@@ -25,7 +25,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -136,8 +135,7 @@ internal fun DateTimePicker(
     
     //reset the selection if/when the dateTime changes
     datePickerState.setSelection(dateTime.dateForPicker)
-    
-    //datePickerState.setSelection(dateTime.dateForPicker)
+
     // create and remember a TimePickerState that resets when dateTime changes
     val timePickerState = rememberSaveable(dateTime, saver = TimePickerState.Saver()) {
         TimePickerState(
