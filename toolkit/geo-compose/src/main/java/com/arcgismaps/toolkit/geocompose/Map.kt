4561188b653f2d6a76897e4d9f08c00b19fcb3f8
Puneet Prakash <Puneet_Prakash@esri.com>
2023-10-10 10:28:48 -0700

    remove unused imports

--- toolkit/geo-compose/src/main/java/com/arcgismaps/toolkit/geocompose/Map.kt
+++ toolkit/geo-compose/src/main/java/com/arcgismaps/toolkit/geocompose/Map.kt
@@ -20,8 +20,6 @@ package com.arcgismaps.toolkit.geocompose
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
