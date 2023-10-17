fd8a4c0cf8936ab640dd6a23bb8d3d6723bdd69d
Puneet Prakash <Puneet_Prakash@esri.com>
2023-10-17 16:34:58 -0700

    add test

--- microapps/FloorFilterApp/app/src/main/java/com/arcgismaps/toolkit/floorfilterapp/screens/MainScreen.kt
+++ microapps/FloorFilterApp/app/src/main/java/com/arcgismaps/toolkit/floorfilterapp/screens/MainScreen.kt
@@ -21,11 +21,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.arcgismaps.mapping.ArcGISMap
+import com.arcgismaps.mapping.MobileMapPackage
 import com.arcgismaps.mapping.PortalItem
 import com.arcgismaps.portal.Portal
 import com.arcgismaps.toolkit.composablemap.ComposableMap
@@ -37,17 +40,34 @@ fun MainScreen() {
     val portalItem = PortalItem(portal, "b4b599a43a474d33946cf0df526426f5")
     val floorAwareWebMap = ArcGISMap(portalItem)
 
-    val mapViewModel = viewModel<MapViewModel>(factory = MapViewModelFactory(floorAwareWebMap))
+    val map by produceState<ArcGISMap?>(initialValue = null) {
+        // this data has no sites, and level.shortName is blank
+        val mmpk = MobileMapPackage(
+            "/data/user/0/com.arcgismaps.toolkit.floorfilterapp/files/Berlin_Kotlin_23.mmpk"
+        )
+        // this data has no sites
+//        val mmpk = MobileMapPackage(
+//            "/data/user/0/com.arcgismaps.toolkit.floorfilterapp/files/sf_mixedlayers_overlay1.mmpk"
+//        )
+        mmpk.load().getOrNull() ?: return@produceState
+        if (mmpk.maps.isEmpty()) return@produceState
+        value = mmpk.maps[0]
+    }
 
-    ComposableMap(
-        modifier = Modifier.fillMaxSize(),
-        mapInterface = mapViewModel
-    ) {
-        Box(
-            modifier = Modifier.fillMaxSize().padding(horizontal = 20.dp, vertical = 40.dp),
-            contentAlignment = Alignment.BottomStart
+//    val mapViewModel = viewModel<MapViewModel>(factory = MapViewModelFactory(floorAwareWebMap))
+    val mapViewModel = map?.let { viewModel<MapViewModel>(factory = MapViewModelFactory(it)) }
+
+    if (mapViewModel != null) {
+        ComposableMap(
+            modifier = Modifier.fillMaxSize(),
+            mapInterface = mapViewModel
         ) {
-            FloorFilter(floorFilterState = mapViewModel.floorFilterState)
+            Box(
+                modifier = Modifier.fillMaxSize().padding(horizontal = 20.dp, vertical = 40.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                FloorFilter(floorFilterState = mapViewModel.floorFilterState)
+            }
         }
     }
 }
