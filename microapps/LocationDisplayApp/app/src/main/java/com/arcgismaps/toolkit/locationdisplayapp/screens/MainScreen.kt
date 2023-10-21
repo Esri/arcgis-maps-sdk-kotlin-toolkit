6f7720bcadbfb02ec90c279e8c5b607691a9e65a
Gunther Heppner <gheppner@esri.com>
2023-10-21 15:44:06 +0200

    screen to start/stop a location display

--- microapps/LocationDisplayApp/app/src/main/java/com/arcgismaps/toolkit/locationdisplayapp/screens/MainScreen.kt
+++ microapps/LocationDisplayApp/app/src/main/java/com/arcgismaps/toolkit/locationdisplayapp/screens/MainScreen.kt
@@ -18,19 +18,94 @@
 
 package com.arcgismaps.toolkit.locationdisplayapp.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Switch
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.arcgismaps.location.LocationDataSourceStatus
 import com.arcgismaps.mapping.ArcGISMap
 import com.arcgismaps.mapping.BasemapStyle
+import com.arcgismaps.mapping.view.LocationDisplay
 import com.arcgismaps.toolkit.geocompose.Map
+import com.arcgismaps.toolkit.geocompose.rememberLocationDisplay
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
+/**
+ * Displays a [Map] with a [LocationDisplay].
+ * The location display can be started.stopped using a [Switch].
+ */
 @Composable
 fun MainScreen() {
+    val scope = rememberCoroutineScope()
     val arcGISMap = remember { ArcGISMap(BasemapStyle.ArcGISImagery) }
+    val checked = remember { mutableStateOf(false) }
+    val locationDisplay = rememberLocationDisplay {
+        start(scope) {
+            checked.value = it.isSuccess
+        }
+    }
 
-    Map(modifier = Modifier.fillMaxSize(),
-        arcGISMap = arcGISMap
-    )
+    Column {
+        Row(modifier = Modifier.padding(5.dp)) {
+            Switch(
+                checked.value,
+                onCheckedChange = {
+                    checked.value = it
+                    if (locationDisplay.isStarted) {
+                        locationDisplay.stop(scope)
+                        checked.value = false
+                    } else {
+                        locationDisplay.start(scope) { result ->
+                            checked.value = result.isSuccess
+                        }
+                    }
+                }
+            )
+        }
+        Map(
+            modifier = Modifier.fillMaxSize(),
+            arcGISMap = arcGISMap,
+            locationDisplay = locationDisplay
+        )
+    }
+}
+
+/**
+ * Extension property on [LocationDisplay] to check its started status.
+ */
+private val LocationDisplay.isStarted: Boolean
+    get() = dataSource.status.value == LocationDataSourceStatus.Started
+
+/**
+ * Extension function on [LocationDisplay] to start [this].
+ * [onCompletion] is called when starting has completed.
+ */
+private fun LocationDisplay.start(
+    scope: CoroutineScope,
+    onCompletion: ((Result<Unit>) -> Unit)? = null
+) {
+    scope.launch {
+        dataSource.start().let { result ->
+            onCompletion?.let {
+                it.invoke(result)
+            }
+        }
+    }
+}
+
+/**
+ * Extension function on [LocationDisplay] to stop [this].
+ */
+private fun LocationDisplay.stop(scope: CoroutineScope) {
+    scope.launch {
+        dataSource.stop()
+    }
 }
