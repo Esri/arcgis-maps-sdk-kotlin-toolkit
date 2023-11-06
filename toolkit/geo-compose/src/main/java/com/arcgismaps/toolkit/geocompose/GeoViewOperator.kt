f991f415e10fa5b1bd900b32d4707b5fdfd08aef
Hudson Miears <hud10837@esri.com>
2023-11-06 18:18:05 +0200

    make MapViewOperator derive from a new GeoViewOperator

---
+++ toolkit/geo-compose/src/main/java/com/arcgismaps/toolkit/geocompose/GeoViewOperator.kt
@@ -0,0 +1,9 @@
+package com.arcgismaps.toolkit.geocompose
+
+/**
+ * Used to perform interactions on a GeoView.
+ *
+ * @since 200.3.0
+ */
+public sealed class GeoViewOperator {
+}
--- toolkit/geo-compose/src/main/java/com/arcgismaps/toolkit/geocompose/MapViewOperator.kt
+++ toolkit/geo-compose/src/main/java/com/arcgismaps/toolkit/geocompose/MapViewOperator.kt
@@ -11,7 +11,7 @@ import com.arcgismaps.mapping.view.ScreenCoordinate
  * There should be a one-to-one relationship between a [MapViewOperator] and a composable [com.arcgismaps.toolkit.geocompose.MapView].
  * Operations can only be performed once the component has entered the composition.
  */
-public class MapViewOperator() {
+public class MapViewOperator : GeoViewOperator() {
 
     /**
      * The [MapView] that this operator will operate on. This should be initialized by the [com.arcgismaps.toolkit.geocompose.MapView]
