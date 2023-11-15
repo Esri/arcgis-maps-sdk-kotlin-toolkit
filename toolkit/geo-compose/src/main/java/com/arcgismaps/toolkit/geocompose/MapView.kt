faa30fd071f3af3cb228e66fd18b4ed862199461
Hudson Miears <hud10837@esri.com>
2023-11-15 13:27:34 +0200

    mv onViewpointChanged parameter

--- toolkit/geo-compose/src/main/java/com/arcgismaps/toolkit/geocompose/MapView.kt
+++ toolkit/geo-compose/src/main/java/com/arcgismaps/toolkit/geocompose/MapView.kt
@@ -63,6 +63,7 @@ import kotlinx.coroutines.launch
  * @param modifier Modifier to be applied to the composable MapView
  * @param arcGISMap the [ArcGISMap] to be rendered by this composable MapView
  * @param viewpointOperation a [MapViewpointOperation] that changes this MapView to a new viewpoint
+ * @param onViewpointChanged lambda invoked when the viewpoint of the composable MapView has changed
  * @param graphicsOverlays the [GraphicsOverlayCollection] used by this composable MapView
  * @param locationDisplay the [LocationDisplay] used by the composable MapView
  * @param geometryEditor the [GeometryEditor] used by the composable MapView to create and edit geometries by user interaction.
@@ -78,7 +79,6 @@ import kotlinx.coroutines.launch
  * @param attributionState specifies the attribution bar's visibility, text changed and layout changed events
  * @param timeExtent the [TimeExtent] used by the composable MapView
  * @param onTimeExtentChanged lambda invoked when the composable MapView's [TimeExtent] is changed
- * @param onViewpointChanged lambda invoked when the viewpoint of the composable MapView has changed
  * @param onNavigationChanged lambda invoked when the navigation status of the composable MapView has changed
  * @param onMapRotationChanged lambda invoked when the rotation of this composable MapView has changed
  * @param onMapScaleChanged lambda invoked when the scale of this composable MapView has changed
@@ -101,6 +101,7 @@ public fun MapView(
     modifier: Modifier = Modifier,
     arcGISMap: ArcGISMap? = null,
     viewpointOperation: MapViewpointOperation? = null,
+    onViewpointChanged: (() -> Unit)? = null,
     graphicsOverlays: GraphicsOverlayCollection = rememberGraphicsOverlayCollection(),
     locationDisplay: LocationDisplay = rememberLocationDisplay(),
     geometryEditor: GeometryEditor? = null,
@@ -115,7 +116,6 @@ public fun MapView(
     attributionState: AttributionState = AttributionState(),
     timeExtent: TimeExtent? = null,
     onTimeExtentChanged: ((TimeExtent?) -> Unit)? = null,
-    onViewpointChanged: (() -> Unit)? = null,
     onNavigationChanged: ((isNavigating: Boolean) -> Unit)? = null,
     onMapRotationChanged: ((Double) -> Unit)? = null,
     onMapScaleChanged: ((Double) -> Unit)? = null,
--- toolkit/geo-compose/src/main/java/com/arcgismaps/toolkit/geocompose/MapViewpointOperation.kt
+++ toolkit/geo-compose/src/main/java/com/arcgismaps/toolkit/geocompose/MapViewpointOperation.kt
@@ -1,3 +1,21 @@
+/*
+ *
+ *  Copyright 2023 Esri
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
 package com.arcgismaps.toolkit.geocompose
 
 import androidx.compose.runtime.Stable
@@ -10,7 +28,7 @@ import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.CompletableDeferred
 
 /**
- * Defines a method for setting the viewpoint of the composable [MapView].
+ * Defines operations for setting the viewpoint of a composable [MapView].
  *
  * @since 200.3.0
  */
@@ -20,9 +38,9 @@ public sealed class MapViewpointOperation {
     private val deferred = CompletableDeferred<Result<Boolean>>()
 
     /**
-     * Awaits the completion of this MapViewpointOperation, ie. when the animation stops.
+     * Awaits the completion of this MapViewpointOperation.
      *
-     * @return A Result returning a bool used to indicate if the operation completed successfully or not
+     * @return a Result returning a boolean used to indicate if the operation completed successfully or not
      * @since 200.3.0
      */
     public suspend fun await(): Result<Boolean> = deferred.await()
@@ -114,7 +132,10 @@ public sealed class MapViewpointOperation {
 
 /**
  * Executes the [MapViewpointOperation] on the given view-based [MapView]. The operation can be awaited using
- * [MapViewpointOperation.await].
+ * [MapViewpointOperation.await]. If the coroutine on which the viewpoint operation is executed is cancelled,
+ * for example due to another viewpoint operation being executed, the [MapViewpointOperation.await] call
+ * will return a Result with a boolean set to `false`, indicating that the viewpoint operation failed
+ * to complete.
  *
  * @param mapView the view-based MapView to execute this operation on
  * @since 200.3.0
