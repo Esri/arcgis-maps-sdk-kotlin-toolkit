9002fac9d1df92278c40f0c7e4075f821f376a13
Puneet Prakash <Puneet_Prakash@esri.com>
2023-11-15 16:10:06 -0800

    Refactor mapview insets app

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -78,7 +78,7 @@ include(":floor-filter-app")
 include(":geo-compose")
 include(":map-compose-app")
 include(":location-display-app")
-include(":mapview-insets-app")
+include(":map-view-insets-app")
 include(":geometry-editor-app")
 
 projects.forEach {
@@ -93,5 +93,5 @@ project(":floor-filter-app").projectDir = File(rootDir, "microapps/FloorFilterAp
 project(":geo-compose").projectDir = File(rootDir, "toolkit/geo-compose")
 project(":map-compose-app").projectDir = File(rootDir, "microapps/mapcomposeapp/app")
 project(":location-display-app").projectDir = File(rootDir, "microapps/locationdisplayapp/app")
-project(":mapview-insets-app").projectDir = File(rootDir, "microapps/mapviewinsetsapp/app")
+project(":map-view-insets-app").projectDir = File(rootDir, "microapps/mapviewinsetsapp/app")
 project(":geometry-editor-app").projectDir = File(rootDir, "microapps/geometryeditorapp/app")
