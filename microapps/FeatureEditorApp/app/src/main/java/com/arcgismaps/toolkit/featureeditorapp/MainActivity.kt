864499d8b98a5e7dc3cd80f1b2815ad27bf3c8ce
Finlay Pearson <fpearson@esri.com>
2023-11-23 17:45:17 +0000

    Formatting

--- microapps/FeatureEditorApp/app/src/main/java/com/arcgismaps/toolkit/featureeditorapp/MainActivity.kt
+++ microapps/FeatureEditorApp/app/src/main/java/com/arcgismaps/toolkit/featureeditorapp/MainActivity.kt
@@ -19,33 +19,20 @@
 package com.arcgismaps.toolkit.featureeditorapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.lifecycle.lifecycleScope
-import com.arcgismaps.ApiKey
 import com.arcgismaps.ArcGISEnvironment
-import com.arcgismaps.data.Feature
-import com.arcgismaps.data.FeatureCollection
-import com.arcgismaps.data.FeatureCollectionTable
-import com.arcgismaps.data.Field
-import com.arcgismaps.data.FieldType
-import com.arcgismaps.geometry.GeometryType
-import com.arcgismaps.geometry.Point
-import com.arcgismaps.geometry.SpatialReference
-import com.arcgismaps.httpcore.authentication.ArcGISAuthenticationChallengeHandler
-import com.arcgismaps.httpcore.authentication.PregeneratedTokenCredential
 import com.arcgismaps.httpcore.authentication.TokenCredential
 import com.arcgismaps.mapping.ArcGISMap
-import com.arcgismaps.mapping.BasemapStyle
-import com.arcgismaps.mapping.layers.FeatureCollectionLayer
 import com.arcgismaps.toolkit.composablemap.MapState
 import com.arcgismaps.toolkit.featureeditorapp.screens.FeatureEditorApp
 import com.arcgismaps.toolkit.featureeditorapp.screens.FeatureEditorAppState
 import com.arcgismaps.toolkit.featureeditorapp.ui.theme.FeatureEditorAppTheme
 import kotlinx.coroutines.launch
 
-private const val WEB_MAP_URL = "https://runtimecoretest.maps.arcgis.com/home/item.html?id=df0f27f83eee41b0afe4b6216f80b541"
+private const val WEB_MAP_URL =
+    "https://runtimecoretest.maps.arcgis.com/home/item.html?id=df0f27f83eee41b0afe4b6216f80b541"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -55,7 +42,13 @@ class MainActivity : ComponentActivity() {
 
         lifecycleScope.launch {
 
-            ArcGISEnvironment.authenticationManager.arcGISCredentialStore.add(TokenCredential.create(WEB_MAP_URL, BuildConfig.WEB_MAP_USERNAME, BuildConfig.WEB_MAP_PASSWORD).getOrThrow())
+            ArcGISEnvironment.authenticationManager.arcGISCredentialStore.add(
+                TokenCredential.create(
+                    WEB_MAP_URL,
+                    BuildConfig.WEB_MAP_USERNAME,
+                    BuildConfig.WEB_MAP_PASSWORD
+                ).getOrThrow()
+            )
 
             val map = ArcGISMap(WEB_MAP_URL)
 
