1289c127ee30daeab2e494b90f9b7640436d31b9
Kaushik Meesala <kmeesala@esri.com>
2023-11-29 13:09:23 -0800

    optimized imports

--- toolkit/featureforms/src/main/java/com/arcgismaps/toolkit/featureforms/components/formelement/GroupElement.kt
+++ toolkit/featureforms/src/main/java/com/arcgismaps/toolkit/featureforms/components/formelement/GroupElement.kt
@@ -37,7 +37,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.toggleableState
 import androidx.compose.ui.state.ToggleableState
