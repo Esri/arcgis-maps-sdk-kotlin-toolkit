959fd7f9c47c7f7a3316f9987c74e68a7b771875
Hudson Miears <hud10837@esri.com>
2023-12-11 13:17:02 +0200

    fix doc and copyright

--- toolkit/geo-compose/src/main/java/com/arcgismaps/toolkit/geocompose/SceneView.kt
+++ toolkit/geo-compose/src/main/java/com/arcgismaps/toolkit/geocompose/SceneView.kt
@@ -1,3 +1,20 @@
+/*
+ *  Copyright 2023 Esri
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
 package com.arcgismaps.toolkit.geocompose
 
 import androidx.compose.runtime.Composable
@@ -12,7 +29,7 @@ import androidx.compose.ui.viewinterop.AndroidView
 import com.arcgismaps.mapping.ArcGISScene
 
 /**
- * A compose equivalent of the view-based [SceneView].
+ * A compose equivalent of the view-based [com.arcgismaps.mapping.view.SceneView].
  *
  * @param modifier Modifier to be applied to the composable SceneView
  * @param arcGISScene the [ArcGISScene] to be rendered by this composable SceneView
@@ -28,7 +45,7 @@ public fun SceneView(
     val sceneView = remember { com.arcgismaps.mapping.view.SceneView(context) }
 
     AndroidView(
-        modifier = modifier.semantics { contentDescription = "MapView" },
+        modifier = modifier.semantics { contentDescription = "SceneView" },
         factory = { sceneView },
         update = {
             it.scene = arcGISScene
