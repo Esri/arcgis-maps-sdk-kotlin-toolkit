d03601b82a2a2a264ca13364018531343f8b7f6d
Shubham Sharma <shubhamsharma@esri.com>
2023-12-12 11:22:51 -0800

    added compose BOM

--- microapps/AuthenticationApp/app/build.gradle.kts
+++ microapps/AuthenticationApp/app/build.gradle.kts
@@ -78,6 +78,7 @@ dependencies {
     implementation(project(":authentication"))
     implementation(project(":composable-map"))
     implementation(arcgis.mapsSdk)
+    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.bundles.composeCore)
     implementation(libs.bundles.core)
     implementation(libs.androidx.lifecycle.runtime.ktx)
