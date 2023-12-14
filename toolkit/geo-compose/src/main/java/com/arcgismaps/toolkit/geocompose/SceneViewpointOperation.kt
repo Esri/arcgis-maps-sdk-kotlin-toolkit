5f855d7caebb5cb2e5618178282db044b643cb03
Hudson Miears <hud10837@esri.com>
2023-12-14 19:04:33 +0200

    add all operations for sceneview

--- toolkit/geo-compose/src/main/java/com/arcgismaps/toolkit/geocompose/SceneViewpointOperation.kt
+++ toolkit/geo-compose/src/main/java/com/arcgismaps/toolkit/geocompose/SceneViewpointOperation.kt
@@ -1,8 +1,12 @@
 package com.arcgismaps.toolkit.geocompose
 
 import androidx.compose.runtime.Stable
+import com.arcgismaps.mapping.Bookmark
+import com.arcgismaps.mapping.Viewpoint
+import com.arcgismaps.mapping.view.Camera
 import com.arcgismaps.mapping.view.SceneView
 import kotlinx.coroutines.CompletableDeferred
+import kotlin.coroutines.cancellation.CancellationException
 
 
 /**
@@ -25,8 +29,56 @@ public sealed class SceneViewpointOperation {
     internal fun complete(result: Result<Boolean>) {
         deferred.complete(result)
     }
+
+    public class Set(public val viewpoint: Viewpoint) : SceneViewpointOperation()
+    public class Animate(
+        public val viewpoint: Viewpoint,
+        public val durationSeconds: Float = 0.25f
+    ) : SceneViewpointOperation()
+    public class SetCamera(public val camera: Camera) : SceneViewpointOperation()
+    public class AnimateCamera(
+        public val camera: Camera,
+        public val durationSeconds: Float = 0.25f
+    ) : SceneViewpointOperation()
+    public class SetBookmark(public val bookmark: Bookmark) : SceneViewpointOperation()
 }
 
 internal suspend fun SceneViewpointOperation.execute(sceneView: SceneView) {
-    TODO()
+    when (this) {
+        is SceneViewpointOperation.Set -> {
+            sceneView.setViewpoint(this.viewpoint)
+            this.complete(Result.success(true))
+        }
+        is SceneViewpointOperation.Animate -> {
+            try {
+                val result = sceneView.setViewpointAnimated(this.viewpoint, this.durationSeconds)
+                this.complete(result)
+            } catch (e: CancellationException) {
+                this.complete(Result.success(false))
+                throw e
+            }
+        }
+        is SceneViewpointOperation.AnimateCamera -> {
+            try {
+                val result = sceneView.setViewpointCameraAnimated(this.camera, this.durationSeconds)
+                this.complete(result)
+            } catch (e: CancellationException) {
+                this.complete(Result.success(false))
+                throw e
+            }
+        }
+        is SceneViewpointOperation.SetBookmark -> {
+            try {
+                val result = sceneView.setBookmark(this.bookmark)
+                this.complete(result)
+            } catch (e: CancellationException) {
+                this.complete(Result.success(false))
+                throw e
+            }
+        }
+        is SceneViewpointOperation.SetCamera -> {
+            sceneView.setViewpointCamera(this.camera)
+            this.complete(Result.success(true))
+        }
+    }
 }
