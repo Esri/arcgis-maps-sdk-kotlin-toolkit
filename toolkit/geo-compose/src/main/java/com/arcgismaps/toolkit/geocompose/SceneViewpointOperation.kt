aa41bb413f638dfd73883ff875aebc4a72886a3b
Hudson Miears <hud10837@esri.com>
2023-12-14 16:51:41 +0200

    create SceneViewpointOperation

---
+++ toolkit/geo-compose/src/main/java/com/arcgismaps/toolkit/geocompose/SceneViewpointOperation.kt
@@ -0,0 +1,27 @@
+package com.arcgismaps.toolkit.geocompose
+
+import androidx.compose.runtime.Stable
+import kotlinx.coroutines.CompletableDeferred
+
+
+/**
+ * Defines operations for setting the viewpoint of a composable [SceneView].
+ *
+ * @since 200.4.0
+ */
+@Stable
+public sealed class SceneViewpointOperation {
+    private val deferred = CompletableDeferred<Result<Boolean>>()
+
+    /**
+     * Awaits the completion of this SceneViewpointOperation.
+     *
+     * @return a Result returning a boolean used to indicate if the operation completed successfully or not
+     * @since 200.4.0
+     */
+    public suspend fun await(): Result<Boolean> = deferred.await()
+
+    internal fun complete(result: Result<Boolean>) {
+        deferred.complete(result)
+    }
+}
