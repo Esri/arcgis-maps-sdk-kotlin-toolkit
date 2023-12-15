6c9e54a11239394c5e87671c1ee7c672828615f2
Hudson Miears <hud10837@esri.com>
2023-12-15 14:34:55 +0200

    add disposal logic to proxy class

--- toolkit/geo-compose/src/main/java/com/arcgismaps/toolkit/geocompose/SceneViewProxy.kt
+++ toolkit/geo-compose/src/main/java/com/arcgismaps/toolkit/geocompose/SceneViewProxy.kt
@@ -12,5 +12,25 @@ package com.arcgismaps.toolkit.geocompose
  * @since 200.4.0
  */
 public class SceneViewProxy : GeoViewProxy("SceneView") {
+    /**
+     * The view-based [com.arcgismaps.mapping.view.SceneView] that this SceneViewProxy will operate on. This should
+     * be initialized by the composable [SceneView] when it enters the composition and set to null when
+     * it is disposed by calling [setSceneView].
+     *
+     * @since 200.4.0
+     */
+    private var sceneView: com.arcgismaps.mapping.view.SceneView? = null
+        set(value) {
+            setGeoView(value)
+        }
 
+    /**
+     * Sets the [sceneView] parameter on this operator. This should be called by the composable [SceneView]
+     * when it enters the composition and set to null when it is disposed.
+     *
+     * @since 200.4.0
+     */
+    internal fun setSceneView(sceneView: com.arcgismaps.mapping.view.SceneView?) {
+        this.sceneView = sceneView
+    }
 }
