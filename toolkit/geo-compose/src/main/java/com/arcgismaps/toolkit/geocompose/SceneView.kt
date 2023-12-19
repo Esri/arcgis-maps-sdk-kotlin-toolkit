54302bd757edb1053dc49110a3de2204d12f8428
hud10837 <hmiears@esri.com>
2023-12-19 16:29:01 +0200

    SceneView: add scene viewpoint operation (#240)
    * create SceneViewpointOperation
    
    * add operation to sceneview
    
    * add all operations for sceneview
    
    * add doc
    
    * eof newline
    
    * eof newline
    
    * Don't use named params on viewpoint updater
    
    * use kotlinx cancellation exception
    
    * add copyright
    
    * use duration in viewpoint operations
    
    * doc
    
    * rename durationSeconds and fix microapp

--- toolkit/geo-compose/src/main/java/com/arcgismaps/toolkit/geocompose/SceneView.kt
+++ toolkit/geo-compose/src/main/java/com/arcgismaps/toolkit/geocompose/SceneView.kt
@@ -48,6 +48,7 @@ import kotlinx.coroutines.launch
  *
  * @param modifier Modifier to be applied to the composable SceneView
  * @param arcGISScene the [ArcGISScene] to be rendered by this composable SceneView
+ * @param viewpointOperation a [SceneViewpointOperation] that changes this SceneView to a new viewpoint
  * @param graphicsOverlays the [GraphicsOverlayCollection] used by this composable SceneView
  * @param sceneViewProxy the [SceneViewProxy] to associate with the composable SceneView
  * @param onInteractingChanged lambda invoked when the user starts and ends interacting with the composable SceneView
@@ -66,6 +67,7 @@ import kotlinx.coroutines.launch
 public fun SceneView(
     modifier: Modifier = Modifier,
     arcGISScene: ArcGISScene? = null,
+    viewpointOperation: SceneViewpointOperation? = null,
     graphicsOverlays: GraphicsOverlayCollection = rememberGraphicsOverlayCollection(),
     sceneViewProxy: SceneViewProxy? = null,
     onInteractingChanged: ((isInteracting: Boolean) -> Unit)? = null,
@@ -105,6 +107,8 @@ public fun SceneView(
         }
     }
 
+    ViewpointUpdater(sceneView, viewpointOperation)
+
     GraphicsOverlaysUpdater(graphicsOverlays, sceneView)
 
     SceneViewEventHandler(
@@ -120,6 +124,22 @@ public fun SceneView(
         onTwoPointerTap,
         onPan,
     )
+}
+
+/**
+ * Updates the viewpoint of the provided view-based [sceneView] using the given [viewpointOperation]. This will be
+ * recomposed when [viewpointOperation] changes.
+ *
+ * @since 200.4.0
+ */
+@Composable
+private fun ViewpointUpdater(
+    sceneView: SceneView,
+    viewpointOperation: SceneViewpointOperation?
+) {
+    LaunchedEffect(viewpointOperation) {
+        viewpointOperation?.execute(sceneView)
+    }
 }
 
 /**
