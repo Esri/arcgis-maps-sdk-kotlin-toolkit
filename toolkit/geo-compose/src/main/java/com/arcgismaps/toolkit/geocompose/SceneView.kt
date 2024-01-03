7e831b62bffbd16a10a6d58ecb683ce4b2902aac
Changan Shi <cshi@esri.com>
2024-01-02 20:10:46 -0800

    shared the ViewpointChangedState with SceneView (#257)

--- toolkit/geo-compose/src/main/java/com/arcgismaps/toolkit/geocompose/SceneView.kt
+++ toolkit/geo-compose/src/main/java/com/arcgismaps/toolkit/geocompose/SceneView.kt
@@ -54,6 +54,7 @@ import kotlinx.coroutines.launch
  * @param modifier Modifier to be applied to the composable SceneView
  * @param arcGISScene the [ArcGISScene] to be rendered by this composable SceneView
  * @param viewpointOperation a [SceneViewpointOperation] that changes this SceneView to a new viewpoint
+ * @param viewpointChangedState specifies lambdas invoked when the viewpoint of the composable SceneView has changed
  * @param graphicsOverlays the [GraphicsOverlayCollection] used by this composable SceneView
  * @param sceneViewProxy the [SceneViewProxy] to associate with the composable SceneView
  * @param sceneViewInteractionOptions the [SceneViewInteractionOptions] used by this composable SceneView
@@ -81,6 +82,7 @@ public fun SceneView(
     modifier: Modifier = Modifier,
     arcGISScene: ArcGISScene? = null,
     viewpointOperation: SceneViewpointOperation? = null,
+    viewpointChangedState: ViewpointChangedState? = null,
     graphicsOverlays: GraphicsOverlayCollection = rememberGraphicsOverlayCollection(),
     sceneViewProxy: SceneViewProxy? = null,
     sceneViewInteractionOptions: SceneViewInteractionOptions = SceneViewInteractionOptions(),
@@ -137,6 +139,7 @@ public fun SceneView(
     GraphicsOverlaysUpdater(graphicsOverlays, sceneView)
 
     AttributionStateHandler(sceneView, attributionState)
+    ViewpointChangedStateHandler(sceneView, viewpointChangedState)
 
     SceneViewEventHandler(
         sceneView,
