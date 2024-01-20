2f4d6534bd5bb41fa6280a5e6ad4cdb5a0e14922
Changan Shi <cshi@esri.com>
2024-01-19 17:13:50 -0800

    Scene view camera controller app (#288)
    * init implementation for camera controller
    
    * add gitignore
    
    * adds comments and remove view model.
    
    * update the doc and png image
    
    * update the comment
    
    * add the extra space line
    
    * update the screenshot
    
    * review update
    
    * update the 2023 comment
    
    * update the comment
    
    * review update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -81,6 +81,7 @@ include(":map-view-insets-app")
 include(":map-view-geometry-editor-app")
 include(":map-view-set-viewpoint-app")
 include(":map-view-identify-app")
+include(":scene-view-camera-controller-app")
 include(":scene-view-lighting-options-app")
 
 projects.forEach {
@@ -98,4 +99,5 @@ project(":map-view-insets-app").projectDir = File(rootDir, "microapps/mapviewins
 project(":map-view-geometry-editor-app").projectDir = File(rootDir, "microapps/mapviewgeometryeditorapp/app")
 project(":map-view-set-viewpoint-app").projectDir = File(rootDir, "microapps/mapviewsetviewpointapp/app")
 project(":map-view-identify-app").projectDir = File(rootDir, "microapps/mapviewidentifyapp/app")
+project(":scene-view-camera-controller-app").projectDir = File(rootDir, "microapps/sceneviewcameracontrollerapp/app")
 project(":scene-view-lighting-options-app").projectDir = File(rootDir, "microapps/sceneviewlightingoptionsapp/app")
