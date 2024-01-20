c9fc2dad979b33a539bbb8ed3bd9f5a117cc2d92
Soren Roth <sor10874@esri.com>
2024-01-19 18:07:39 -0800

    conclude merge

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,9 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.gradle.secrets) apply false
+    alias(libs.plugins.kapt) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.hilt) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.dokka) apply false
 }
