9748263edf6aaf71a20713e7e1b035860430451b
hud10837 <hmiears@esri.com>
2024-04-25 11:26:00 +0300

    Separate creation of component and microapps (#395)
    * prompt for name
    
    * separate new microapp into own script
    
    * eof newline
    
    * fix plugins block
    
    * rm dependency from template app
    
    * fix reference to script in readme
    
    * update readme
    
    * add components and microapps to settings automatically
    
    * update help dialogs
    
    * update copyright year
    
    * update settings.gradle

--- microapps/TemplateApp/app/build.gradle.kts
+++ microapps/TemplateApp/app/build.gradle.kts
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright 2023 Esri
+ *  Copyright 2024 Esri
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -80,7 +80,6 @@ tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class).all {
 }
 
 dependencies {
-    implementation(project(":template"))
     implementation(project(":geoview-compose"))
     implementation(arcgis.mapsSdk)
     implementation(platform(libs.androidx.compose.bom))
