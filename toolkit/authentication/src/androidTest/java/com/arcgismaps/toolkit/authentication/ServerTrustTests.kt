088e87841d088ebd67f0891118c09aeb3169e942
Hudson Miears <hud10837@esri.com>
2024-04-29 11:12:08 +0300

    Add doc

--- toolkit/authentication/src/androidTest/java/com/arcgismaps/toolkit/authentication/ServerTrustTests.kt
+++ toolkit/authentication/src/androidTest/java/com/arcgismaps/toolkit/authentication/ServerTrustTests.kt
@@ -1,3 +1,19 @@
+/*
+ *  Copyright 2024 Esri
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
 package com.arcgismaps.toolkit.authentication
 
 import androidx.compose.ui.test.assertIsDisplayed
@@ -35,8 +51,18 @@ class ServerTrustTests {
         }
     }
 
+    /**
+     * Given a Dialog Authenticator
+     * When a server trust challenge is issued
+     * Then the dialog prompt should be displayed
+     *
+     * When the user trusts the server
+     * Then the dialog should be dismissed
+     *
+     * @since 200.4.0
+     */
     @Test
-    fun promptIsDisplayed() = runTest {
+    fun trustSelfSignedCertificate_4_1() = runTest {
         composeTestRule.setContent {
             DialogAuthenticator(authenticatorState = authenticatorState)
         }
