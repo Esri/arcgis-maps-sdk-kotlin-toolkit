e5f0a0a70826efc7126869b9c416ee247792404c
Shubham Sharma <shubhamsharma@esri.com>
2024-05-07 13:25:50 -0700

    Revert "removed scaffold"
    This reverts commit 6dbe0ec3c10ae71f771b43a73fd9e4a857b0f26a.

--- microapps/FloorFilterApp/app/src/main/java/com/arcgismaps/toolkit/floorfilterapp/MainActivity.kt
+++ microapps/FloorFilterApp/app/src/main/java/com/arcgismaps/toolkit/floorfilterapp/MainActivity.kt
@@ -20,7 +20,13 @@ package com.arcgismaps.toolkit.floorfilterapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.arcgismaps.ApiKey
 import com.arcgismaps.ArcGISEnvironment
@@ -40,9 +46,12 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FloorFilterApp() {
-    MainScreen()
+    Scaffold(topBar = { TopAppBar(title = { Text("FloorFilter App") }) }) {
+        MainScreen(Modifier.padding(it))
+    }
 }
 
 @Preview(showBackground = true)
