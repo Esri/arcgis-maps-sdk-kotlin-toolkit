f21d9d5168458794eb045cf059d1443a004933af
Shubham Sharma <shubhamsharma@esri.com>
2024-05-13 12:31:27 -0700

    Add a popup with popupPositionProvider

--- toolkit/geoview-compose/src/main/java/com/arcgismaps/toolkit/geoviewcompose/Callout.kt
+++ toolkit/geoview-compose/src/main/java/com/arcgismaps/toolkit/geoviewcompose/Callout.kt
@@ -34,16 +34,23 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntRect
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.times
+import androidx.compose.ui.window.Popup
+import androidx.compose.ui.window.PopupPositionProvider
+import androidx.compose.ui.window.PopupProperties
 import com.arcgismaps.LoadStatus
 import com.arcgismaps.geometry.Point
 import com.arcgismaps.mapping.view.MapView
 import com.arcgismaps.mapping.view.ScreenCoordinate
+import kotlin.math.roundToInt
 
 /**
  * The receiver class of the MapView content lambda.
@@ -90,23 +97,48 @@ public fun MapViewScope.Callout(
     if (mapView.map?.loadStatus?.collectAsState()?.value == LoadStatus.Loaded) {
         val calloutScreenCoordinate: ScreenCoordinate = mapView.locationToScreen(location)
 
-        Box(
-            modifier = modifier
-                .drawCalloutShape(
-                    cornerRadius = with(localDensity) { cornerRadius.toPx() },
-                    anchorLeaderWidth = with(localDensity) { anchorLeaderWidth.toPx() },
-                    anchorLeaderHeight = with(localDensity) { anchorLeaderHeight.toPx() },
-                    strokeBorderWidth = with(localDensity) { strokeBorderWidth.toPx() },
-                    minSize = minSize,
-                    strokeColor = stokeColor,
-                    calloutBackgroundColor = calloutBackgroundColor,
-                    calloutScreenCoordinate = calloutScreenCoordinate,
-                    calloutContentPadding = calloutContentPadding
+        val popupPositionProvider = object : PopupPositionProvider {
+            override fun calculatePosition(
+                anchorBounds: IntRect,
+                windowSize: IntSize,
+                layoutDirection: LayoutDirection,
+                popupContentSize: IntSize
+            ): IntOffset {
+                return IntOffset(
+                    x = calloutScreenCoordinate.x.roundToInt() - (popupContentSize.width / 2),
+                    y = calloutScreenCoordinate.y.roundToInt()
+                            - (popupContentSize.height / 2)
+                            - (with(localDensity) { anchorLeaderHeight.toPx() }).roundToInt()
                 )
-        )
-        {
-            this.content()
+            }
         }
+
+        Popup(
+            popupPositionProvider = popupPositionProvider,
+            onDismissRequest = {
+                // TODO
+            },
+            properties = PopupProperties()
+        ) {
+            Box(
+                modifier = modifier
+                    .drawCalloutShape(
+                        cornerRadius = with(localDensity) { cornerRadius.toPx() },
+                        anchorLeaderWidth = with(localDensity) { anchorLeaderWidth.toPx() },
+                        anchorLeaderHeight = with(localDensity) { anchorLeaderHeight.toPx() },
+                        strokeBorderWidth = with(localDensity) { strokeBorderWidth.toPx() },
+                        minSize = minSize,
+                        strokeColor = stokeColor,
+                        calloutBackgroundColor = calloutBackgroundColor,
+                        calloutContentPadding = calloutContentPadding,
+                    )
+            )
+            {
+                this.content()
+            }
+        }
+
+
     }
 }
 
@@ -120,7 +152,6 @@ public fun MapViewScope.Callout(
  * [strokeColor] Color used to define the outline stroke.
  * [calloutBackgroundColor] Color used to define the fill color of the Callout shape.
  * [minSize] Minimum size the of the Callout shape.
- * [calloutScreenCoordinate] Represents the x,y coordinate of the callout leader.
  * @since 200.5.0
  */
 @Composable
@@ -132,16 +163,9 @@ private fun Modifier.drawCalloutShape(
     strokeColor: Color,
     calloutBackgroundColor: Color,
     minSize: DpSize,
-    calloutScreenCoordinate: ScreenCoordinate,
     calloutContentPadding: PaddingValues
 ) = then(
     sizeIn(minWidth = minSize.width, minHeight = minSize.height)
-        // Set bottom padding to ensure the leader is visible
-        .padding(bottom = with(LocalDensity.current) { anchorLeaderHeight.toDp() })
-        .graphicsLayer {
-            translationX = calloutScreenCoordinate.x.toFloat()
-            translationY = calloutScreenCoordinate.y.toFloat()
-        }
         .drawWithCache {
             onDrawBehind {
                 // Define the Path of the callout
@@ -305,8 +329,7 @@ public fun CalloutPreview() {
                     minSize = minSize,
                     strokeColor = stokeColor,
                     calloutBackgroundColor = calloutBackgroundColor,
-                    calloutScreenCoordinate = ScreenCoordinate(0.0, 0.0),
-                    calloutContentPadding = calloutContentPadding
+                    calloutContentPadding = calloutContentPadding,
                 )
         )
         {
