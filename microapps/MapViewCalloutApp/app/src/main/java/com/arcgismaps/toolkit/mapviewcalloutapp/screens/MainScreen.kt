1164eee709e81b82e45eda3bdec75bc11525537f
Puneet Prakash <puneet_prakash@esri.com>
2024-05-13 09:01:36 -0700

    revert changes to micro app

--- microapps/MapViewCalloutApp/app/src/main/java/com/arcgismaps/toolkit/mapviewcalloutapp/screens/MainScreen.kt
+++ microapps/MapViewCalloutApp/app/src/main/java/com/arcgismaps/toolkit/mapviewcalloutapp/screens/MainScreen.kt
@@ -22,56 +22,23 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.DpOffset
 import com.arcgismaps.toolkit.geoviewcompose.Callout
 import com.arcgismaps.toolkit.geoviewcompose.MapView
-import com.arcgismaps.mapping.view.DoubleXY
-import com.arcgismaps.mapping.view.Graphic
-import com.arcgismaps.mapping.view.GraphicsOverlay
-import com.arcgismaps.mapping.symbology.SimpleMarkerSymbol
-import com.arcgismaps.mapping.symbology.SimpleMarkerSymbolStyle
 
 @Composable
 fun MainScreen(viewModel: MapViewModel) {
-
-    val graphicsOverlay = GraphicsOverlay()
-    val graphicsOverlays by remember {
-        mutableStateOf(
-            listOf(
-                graphicsOverlay
-            )
-        )
-    }
 
     val mapPoint = viewModel.mapPoint.collectAsState().value
 
     MapView(
         modifier = Modifier.fillMaxSize(),
         arcGISMap = viewModel.arcGISMap,
-        graphicsOverlays = graphicsOverlays,
-        onSingleTapConfirmed = {
-            graphicsOverlay.graphics.clear()
-            viewModel.setMapPoint(it)
-            val simpleGraphic =
-                Graphic(
-                    it.mapPoint,
-//                    mapPoint,
-                    SimpleMarkerSymbol(
-                        style = SimpleMarkerSymbolStyle.Circle,
-                        color = com.arcgismaps.Color.black
-                    )
-                )
-            graphicsOverlay.graphics.add(simpleGraphic)
-        },
+        onSingleTapConfirmed = viewModel::setMapPoint,
         content = if (mapPoint != null) {
             {
-                Callout(location = mapPoint, offset = DoubleXY(200.0, 0.0), rotateOffsetWithGeoView = true){
-//                Callout(location = mapPoint, offset = DpOffset(200.0, 0.0), rotateOffsetWithGeoView = true){
+                Callout(location = mapPoint) {
                     Text(
                         "Hello, World!",
                         color = Color.Green
