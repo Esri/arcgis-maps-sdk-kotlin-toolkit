3579299246dc179387d6d4c525e12f332e5e33aa
Gunther Heppner <gheppner@esri.com>
2024-05-21 12:46:11 +0200

    add comment

--- ci/test/build.sh
+++ ci/test/build.sh
@@ -7,6 +7,9 @@ echo "$(pwd)"
 echo ${apps_path}
 function _build() {
   _log "Run the gradle assembleRelease task to pre-build the Kotlin Toolkit"
+  # This is useful because if gradle returns a non-zero exit code it means we are not even able to build the API. In that
+  # case, we can fail the test job without having to pull the test data (which could potentially take a long time).
+  # See https://devtopia.esri.com/runtime/kotlin/pull/775#discussion_r801362
   if ! ./gradlew assembleRelease; then
     echo "error: Running the assembleRelease gradle task failed"
     exit 1
