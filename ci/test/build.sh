88a13741bd069d0796a31cdfda646445ce25445e
Gunther Heppner <gheppner@esri.com>
2024-05-22 21:12:37 +0200

    remove echo debugging

--- ci/test/build.sh
+++ ci/test/build.sh
@@ -3,8 +3,6 @@
 source $(dirname ${BASH_SOURCE})/../common.sh
 
 cd ${apps_path}/arcgis-maps-sdk-kotlin-toolkit
-echo "$(pwd)"
-echo ${apps_path}
 function _build() {
   _log "Run the gradle assembleRelease task to pre-build the Kotlin Toolkit"
   # This is useful because if gradle returns a non-zero exit code it means we are not even able to build the API. In that
