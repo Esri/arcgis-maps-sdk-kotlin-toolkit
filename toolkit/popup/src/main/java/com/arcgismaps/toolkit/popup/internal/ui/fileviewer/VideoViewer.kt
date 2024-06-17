4d30969b08841c2a944d381208e0f083d9b67017
Erick <Erick_Solis@esri.com>
2024-06-17 16:12:12 -0700

    update

--- toolkit/popup/src/main/java/com/arcgismaps/toolkit/popup/internal/ui/fileviewer/VideoViewer.kt
+++ toolkit/popup/src/main/java/com/arcgismaps/toolkit/popup/internal/ui/fileviewer/VideoViewer.kt
@@ -17,11 +17,7 @@ package com.arcgismaps.toolkit.popup.internal.ui.fileviewer
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.media3.common.MediaItem
@@ -31,8 +27,6 @@ import androidx.media3.ui.PlayerView
 @Composable
 internal fun VideoViewer(path: String) {
     val context = LocalContext.current
-
-    // create the player
     val exoPlayer = remember {
         ExoPlayer.Builder(context).build().apply {
             val mediaItem = MediaItem.Builder()
@@ -45,7 +39,6 @@ internal fun VideoViewer(path: String) {
 
     AndroidView(
         factory = {
-            // exo player view for our video player
             PlayerView(context).apply {
                 player = exoPlayer
             }
@@ -53,7 +46,6 @@ internal fun VideoViewer(path: String) {
     )
     DisposableEffect(Unit) {
         onDispose {
-            // release player when no longer needed
             exoPlayer.release()
         }
     }
