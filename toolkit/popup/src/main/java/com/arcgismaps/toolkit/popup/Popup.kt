4d9fa733be732fb3dc5fd5372399591b578dfd54
Soren Roth <sor10874@esri.com>
2024-07-11 12:50:41 -0700

    Set the color of the Popup title text

--- toolkit/popup/src/main/java/com/arcgismaps/toolkit/popup/Popup.kt
+++ toolkit/popup/src/main/java/com/arcgismaps/toolkit/popup/Popup.kt
@@ -34,6 +34,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -168,6 +169,7 @@ private fun Popup(popupState: PopupState, initialized: Boolean, refreshed: Boole
     ) {
         Text(
             text = popup.title,
+            color = MaterialTheme.colorScheme.onBackground,
             modifier = Modifier.padding(horizontal = 15.dp)
         )
         Spacer(
