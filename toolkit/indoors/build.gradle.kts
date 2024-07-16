29ab8c5a427c182d6d6dd01a5837ad38c4f46984
Hudson Miears <hud10837@esri.com>
2024-07-16 17:57:05 +0300

    add targetSdk to indoors testOptions

--- toolkit/indoors/build.gradle.kts
+++ toolkit/indoors/build.gradle.kts
@@ -48,6 +48,7 @@ android {
      * folder in the project's root directory.
      */
     testOptions {
+        targetSdk = libs.versions.compileSdk.get().toInt()
         val connectedTestReportsPath: String by project
         reportDir = "$connectedTestReportsPath/${project.name}"
     }
