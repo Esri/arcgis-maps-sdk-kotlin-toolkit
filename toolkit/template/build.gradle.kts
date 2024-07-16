429190757b8e7f71d8c9a58cb492af311f5dfb9e
Hudson Miears <hud10837@esri.com>
2024-07-16 18:58:59 +0300

    add lint targetsdk

--- toolkit/template/build.gradle.kts
+++ toolkit/template/build.gradle.kts
@@ -69,6 +69,9 @@ android {
         val connectedTestReportsPath: String by project
         reportDir = "$connectedTestReportsPath/${project.name}"
     }
+    lint {
+        targetSdk = libs.versions.compileSdk.get().toInt()
+    }
 }
 
 dependencies {
