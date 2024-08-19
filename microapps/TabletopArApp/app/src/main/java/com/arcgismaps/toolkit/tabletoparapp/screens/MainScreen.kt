cff0534b9f8cda9d9d8b9087b597559d5d1fafcc
Hudson Miears <hud10837@esri.com>
2024-08-19 16:02:52 +0300

    update microapp to use tabletop proxy

--- microapps/TabletopArApp/app/src/main/java/com/arcgismaps/toolkit/tabletoparapp/screens/MainScreen.kt
+++ microapps/TabletopArApp/app/src/main/java/com/arcgismaps/toolkit/tabletoparapp/screens/MainScreen.kt
@@ -20,18 +20,48 @@ package com.arcgismaps.toolkit.tabletoparapp.screens
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import com.arcgismaps.geometry.Point
 import com.arcgismaps.mapping.ArcGISScene
 import com.arcgismaps.mapping.BasemapStyle
 import com.arcgismaps.mapping.Viewpoint
 import com.arcgismaps.toolkit.ar.TableTopSceneView
+import com.arcgismaps.toolkit.ar.TableTopSceneViewProxy
+import kotlin.math.roundToInt
 
 @Composable
 fun MainScreen() {
-    val arcGISScene = remember { ArcGISScene(BasemapStyle.ArcGISImagery).apply { initialViewpoint = Viewpoint(34.056295, -117.195800, 10000000.0) } }
+    val arcGISScene = remember {
+        ArcGISScene(BasemapStyle.ArcGISImagery).apply {
+            initialViewpoint = Viewpoint(34.056295, -117.195800, 10000000.0)
+        }
+    }
+    val tableTopSceneViewProxy = remember { TableTopSceneViewProxy() }
+    var tappedLocation by remember { mutableStateOf<Point?>(null) }
     Box(modifier = Modifier.fillMaxSize()) {
-        TableTopSceneView(arcGISScene = arcGISScene, modifier = Modifier.fillMaxSize())
+        TableTopSceneView(
+            arcGISScene = arcGISScene,
+            modifier = Modifier.fillMaxSize(),
+            tableTopSceneViewProxy = tableTopSceneViewProxy,
+            onSingleTapConfirmed = {
+                val location = tableTopSceneViewProxy.screenToBaseSurface(it.screenCoordinate)
+                location?.let { point ->
+                    tappedLocation = point
+                }
+            }
+        ) {
+            tappedLocation?.let {
+                Callout(location = it, modifier = Modifier.wrapContentSize()) {
+                    Text("Lat: ${it.y.roundToInt()}, Lon: ${it.x.roundToInt()}")
+                }
+            }
+        }
     }
 }
