785e3f70d28882fe77394b00f33a3f90bf672c62
Soren Roth <sor10874@esri.com>
2024-08-19 12:44:11 -0700

    whitespace

--- microapps/UtilityNetworkTraceApp/app/src/main/java/com/arcgismaps/toolkit/utilitynetworktraceapp/MainActivity.kt
+++ microapps/UtilityNetworkTraceApp/app/src/main/java/com/arcgismaps/toolkit/utilitynetworktraceapp/MainActivity.kt
@@ -50,8 +50,6 @@ import com.esri.microappslib.theme.MicroAppTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
         setContent {
             MicroAppTheme {
                 UtilityNetworkTraceApp()
