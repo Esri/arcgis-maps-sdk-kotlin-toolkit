d9499e15dd75c65327e285201fda26ec2db22505
Erick <Erick_Solis@esri.com>
2024-08-21 16:25:26 -0700

    small edits

--- toolkit/sharedlib/src/main/java/com/arcgismaps/toolkit/ui/ExpandableCard.kt
+++ toolkit/sharedlib/src/main/java/com/arcgismaps/toolkit/ui/ExpandableCard.kt
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2024 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.arcgismaps.toolkit.ui
 
 import androidx.compose.animation.AnimatedVisibility
@@ -58,7 +73,7 @@ public fun ExpandableCard(
             .padding(shapes.padding)
     ) {
         Column {
-            ExpandableHeader2(
+            ExpandableHeader(
                 title = title,
                 description = description,
                 expandable = toggleable,
@@ -79,7 +94,7 @@ public fun ExpandableCard(
 }
 
 @Composable
-private fun ExpandableHeader2(
+private fun ExpandableHeader(
     title: String = "",
     description: String = "",
     expandable: Boolean,
@@ -142,7 +157,7 @@ private fun ExpandableHeader2(
 @Preview
 @Composable
 internal fun ExpandableHeaderPreview() {
-    ExpandableHeader2(
+    ExpandableHeader(
         title = "The Title",
         colors = ExpandableCardDefaults.colors(),
         description = "the description",
