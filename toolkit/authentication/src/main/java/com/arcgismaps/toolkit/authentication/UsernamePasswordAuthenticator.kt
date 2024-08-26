a0102b53c88271d07f65ce0e6f41ed7c49395962
Nour Siwar <nour.siwar@outlook.com>
2024-08-26 16:24:42 -0700

    Added dialog change

--- toolkit/authentication/src/main/java/com/arcgismaps/toolkit/authentication/UsernamePasswordAuthenticator.kt
+++ toolkit/authentication/src/main/java/com/arcgismaps/toolkit/authentication/UsernamePasswordAuthenticator.kt
@@ -19,9 +19,7 @@
 package com.arcgismaps.toolkit.authentication
 
 import android.net.Uri
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -56,7 +54,6 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogProperties
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 
@@ -106,8 +103,7 @@ public fun UsernamePasswordAuthenticator(
             title = {
                 Text(
                     text = "Authentication Required",
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier.border(BorderStroke(2.dp, Color.Red))
+                    textAlign = TextAlign.Center
                 )
             },
             text = {
