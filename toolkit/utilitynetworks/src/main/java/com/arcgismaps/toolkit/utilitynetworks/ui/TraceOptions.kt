478283c1274cc4fe932fa93c6f158b41bed1f5bf
Soren Roth <sor10874@esri.com>
2024-08-30 14:11:54 -0700

    remove print statement

--- toolkit/utilitynetworks/src/main/java/com/arcgismaps/toolkit/utilitynetworks/ui/TraceOptions.kt
+++ toolkit/utilitynetworks/src/main/java/com/arcgismaps/toolkit/utilitynetworks/ui/TraceOptions.kt
@@ -205,7 +205,6 @@ private fun StartingPointsEditor() {
                 startingPoints.forEach {
                     val row = StartingPointRowData(name = it.name)
                     StartingPointRow(row) {
-                        println("removing row with name ${row.name}")
                         startingPoints.remove(row)
                         counter -= 1
                     }
