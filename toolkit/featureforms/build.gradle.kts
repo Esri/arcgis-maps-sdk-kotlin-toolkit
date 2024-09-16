8bb7966d922bad0e305815ed0acc0a359cc2d4bd
Kaushik Meesala <kaushik.pulagara@gmail.com>
2024-09-16 14:30:33 -0700

    merge `Feature branches/forms` into v.next (#566)
    * `Forms`: Add `TextFormElement` (#542)
    
    * `Forms`: Add `TextFormElement` tests (#551)
    
    * add tests
    
    * updated feature form doc
    
    * bump sdk version
    
    * `Forms` : Add SubTypeFeatureLayer support (#559)
    
    * `Forms`: Fix stale `LaunchedEffect`s (#563)
    
    * fix stale launched effects
    
    * use rememberupdatedstate
    
    * update feature form doc (#565)

--- toolkit/featureforms/build.gradle.kts
+++ toolkit/featureforms/build.gradle.kts
@@ -95,7 +95,8 @@ apiValidation {
         "com.arcgismaps.toolkit.featureforms.internal.components.datetime.picker.time.ComposableSingletons\$TimePickerKt",
         "com.arcgismaps.toolkit.featureforms.internal.components.attachment.ComposableSingletons\$AttachmentFormElementKt",
         "com.arcgismaps.toolkit.featureforms.internal.components.attachment.ComposableSingletons\$AttachmentTileKt",
-        "com.arcgismaps.toolkit.featureforms.internal.components.formelement.ComposableSingletons\$GroupElementKt"
+        "com.arcgismaps.toolkit.featureforms.internal.components.formelement.ComposableSingletons\$GroupElementKt",
+        "com.arcgismaps.toolkit.featureforms.internal.components.text.ComposableSingletons\$TextFormElementKt",
     )
     
     ignoredClasses.addAll(composableSingletons)
@@ -104,6 +105,7 @@ apiValidation {
 
 dependencies {
     api(arcgis.mapsSdk)
+    implementation(libs.bundles.commonmark)
     implementation(platform(libs.coil.bom))
     implementation(libs.coil.compose)
     implementation(platform(libs.androidx.compose.bom))
