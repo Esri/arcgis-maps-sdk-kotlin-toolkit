d57ca1f616b407bca25e92a35c8bb4038e67c088
Hudson Miears <hud10837@esri.com>
2024-10-04 17:56:47 +0300

    display simple helper text while detecting planes

--- microapps/ArTabletopApp/app/src/main/java/com/arcgismaps/toolkit/artabletopapp/screens/MainScreen.kt
+++ microapps/ArTabletopApp/app/src/main/java/com/arcgismaps/toolkit/artabletopapp/screens/MainScreen.kt
@@ -18,7 +18,10 @@
 
 package com.arcgismaps.toolkit.artabletopapp.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
@@ -28,6 +31,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -36,7 +40,6 @@ import com.arcgismaps.geometry.Point
 import com.arcgismaps.geometry.SpatialReference
 import com.arcgismaps.mapping.ArcGISScene
 import com.arcgismaps.mapping.BasemapStyle
-import com.arcgismaps.mapping.Viewpoint
 import com.arcgismaps.toolkit.ar.TableTopSceneView
 import com.arcgismaps.toolkit.ar.TableTopSceneViewProxy
 import com.arcgismaps.toolkit.ar.TableTopSceneViewStatus
@@ -72,6 +75,17 @@ fun MainScreen() {
                 Callout(location = it, modifier = Modifier.wrapContentSize()) {
                     Text("Lat: ${it.y.roundToInt()}, Lon: ${it.x.roundToInt()}")
                 }
+            }
+        }
+        if (initializationStatus == TableTopSceneViewStatus.DetectingPlanes) {
+            Column(
+                modifier = Modifier
+                    .background(Color.Gray.copy(alpha = 0.5f))
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "Move your phone around to detect planes...")
             }
         }
         Text(
