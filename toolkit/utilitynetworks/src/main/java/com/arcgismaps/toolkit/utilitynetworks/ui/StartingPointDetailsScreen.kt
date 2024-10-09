82a72528ff72ae9b989d2026127e57ee7290d8d0
Puneet Prakash <puneet_prakash@esri.com>
2024-10-09 14:47:51 -0700

    WIP

--- toolkit/utilitynetworks/src/main/java/com/arcgismaps/toolkit/utilitynetworks/ui/StartingPointDetailsScreen.kt
+++ toolkit/utilitynetworks/src/main/java/com/arcgismaps/toolkit/utilitynetworks/ui/StartingPointDetailsScreen.kt
@@ -29,8 +29,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -46,6 +49,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import com.arcgismaps.toolkit.utilitynetworks.R
 import com.arcgismaps.toolkit.utilitynetworks.StartingPoint
@@ -53,7 +58,11 @@ import com.arcgismaps.toolkit.utilitynetworks.internal.util.Title
 import com.arcgismaps.toolkit.utilitynetworks.ui.material3.Slider
 import com.arcgismaps.utilitynetworks.UtilityNetworkSourceType
 
-
+/**
+ * A composable screen that shows the details of a starting point for a trace.
+ *
+ * @since 200.6.0
+ */
 @Composable
 internal fun StartingPointDetailsScreen(startingPoint: StartingPoint,
                                         onZoomToResults: () -> Unit,
@@ -70,32 +79,47 @@ internal fun StartingPointDetailsScreen(startingPoint: StartingPoint,
                 horizontalArrangement = Arrangement.SpaceBetween,
                 verticalAlignment = Alignment.CenterVertically
             ) {
-            Image(
-                imageVector = Icons.AutoMirrored.Default.ArrowBack,
-                contentDescription = "back"
-            )
-            ReadOnlyTextField(
-                text = stringResource(id = R.string.starting_points))
+                Image(
+                    imageVector = Icons.AutoMirrored.Default.ArrowBack,
+                    contentDescription = "back"
+                )
+                Text(
+                    text = stringResource(id = R.string.starting_points),
+                    style = MaterialTheme.typography.titleMedium,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
             }
 
-            Spacer(modifier = Modifier
-                .fillMaxWidth()
-                .height(10.dp))
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(10.dp)
+            )
 
             Title(startingPoint.name, onZoomToResults, onClearAllResults)
 
-            if (startingPoint.utilityElement.networkSource.sourceType == UtilityNetworkSourceType.Edge) {
-                FractionAlongEdgeSlider(startingPoint, onFractionChanged)
-            } else if (startingPoint.utilityElement.networkSource.sourceType == UtilityNetworkSourceType.Junction
-                && startingPoint.utilityElement.terminal != null
-                && !startingPoint.utilityElement.assetType.terminalConfiguration?.terminals.isNullOrEmpty()
+            LazyColumn(
+                modifier = Modifier
+                    .padding(10.dp, 3.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                TerminalConfiguration(startingPoint)
+                if (startingPoint.utilityElement.networkSource.sourceType == UtilityNetworkSourceType.Edge) {
+                    item {
+                        FractionAlongEdgeSlider(startingPoint, onFractionChanged)
+                    }
+                } else if (startingPoint.utilityElement.networkSource.sourceType == UtilityNetworkSourceType.Junction
+                    && startingPoint.utilityElement.terminal != null
+                    && !startingPoint.utilityElement.assetType.terminalConfiguration?.terminals.isNullOrEmpty()
+                ) {
+                    item {
+                        TerminalConfiguration(startingPoint)
+                    }
+                }
+                item {
+                    Attributes(startingPoint)
+                }
             }
-            Spacer(modifier = Modifier
-                .fillMaxWidth()
-                .height(10.dp))
-            Attributes(startingPoint)
         }
     }
 }
@@ -128,38 +152,49 @@ private fun FractionAlongEdgeSlider(startingPoint: StartingPoint, onFractionChan
 @Composable
 private fun Attributes(startingPoint: StartingPoint) {
     Column {
-        Text(text = stringResource(id = R.string.attributes),
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(10.dp)
+        )
+        Text(
+            text = stringResource(id = R.string.attributes),
             color = Color.Gray,
             style = MaterialTheme.typography.titleMedium,
-            modifier = Modifier.padding(start = 16.dp))
-        Box(modifier = Modifier
-            .padding(horizontal = 16.dp)
-            .fillMaxWidth()
-            .background(color = MaterialTheme.colorScheme.background)) {
-            LazyColumn(
-                modifier = Modifier
-                    .padding(10.dp, 3.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                startingPoint.feature.attributes.toSortedMap().forEach() { attribute ->
-                    item {
-                        Row(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(vertical = 3.dp),
-                            horizontalArrangement = Arrangement.SpaceBetween
-                        ) {
-                            Text(
-                                text = attribute.key,
-                                style = MaterialTheme.typography.bodyLarge,
-                            )
-                            Text(
-                                text = attribute.value as? String ?: "",
-                                style = MaterialTheme.typography.bodyLarge,
-                            )
-                        }
-                        HorizontalDivider(modifier = Modifier.padding(vertical = 4.dp))
-                    }
+            modifier = Modifier.padding(start = 16.dp)
+        )
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .fillMaxWidth()
+                .background(color = MaterialTheme.colorScheme.background)
+        ) {
+            val attributes = startingPoint.feature.attributes.toSortedMap().toList()
+            attributes.forEachIndexed { index, attribute ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 3.dp)
+                        .background(color = MaterialTheme.colorScheme.background),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        modifier = Modifier.padding(start = 5.dp),
+                        text = attribute.first,
+                        style = MaterialTheme.typography.bodyLarge,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                    Text(
+                        modifier = Modifier.padding(end = 5.dp),
+                        text = attribute.second as? String ?: "",
+                        style = MaterialTheme.typography.bodyLarge,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                }
+                if (index < attributes.size - 1) {
+                    HorizontalDivider(modifier = Modifier.padding(horizontal = 5.dp))
                 }
             }
         }
@@ -179,36 +214,36 @@ private fun TerminalConfiguration(startingPoint: StartingPoint) {
             color = Color.Gray,
             style = MaterialTheme.typography.titleMedium,
             modifier = Modifier.padding(start = 16.dp))
-//        MaterialTheme(shapes = MaterialTheme.shapes.copy(extraSmall = RoundedCornerShape(16.dp))) {
-//            DropdownMenu(
-//                expanded = showDropdown,
-//                offset = DpOffset.Zero,
-//                onDismissRequest = { showDropdown = false }) {
-//                startingPoint.utilityElement.assetType.terminalConfiguration?.terminals?.forEachIndexed { index, utilityTerminal ->
-//                    DropdownMenuItem(
-//                        text = {
-//                            ReadOnlyTextField(
-//                                text = utilityTerminal.name, leadingIcon = if (utilityTerminal.name == selectedConfigName) {
-//                                    {
-//                                        Icon(
-//                                            imageVector = Icons.Filled.Done,
-//                                            contentDescription = "Done icon",
-//                                            modifier = Modifier.size(FilterChipDefaults.IconSize)
-//                                        )
-//                                    }
-//                                } else {
-//                                    null
-//                                }
-//                            )
-//                        },
-//                        onClick = {
-//                            onTraceSelected(index)
-//                            showDropdown = false
-//                        },
-//                        contentPadding = PaddingValues(vertical = 0.dp, horizontal = 10.dp)
-//                    )
-//                }
-//            }
-//        }
+        MaterialTheme(shapes = MaterialTheme.shapes.copy(extraSmall = RoundedCornerShape(16.dp))) {
+            DropdownMenu(
+                expanded = showDropdown,
+                offset = DpOffset.Zero,
+                onDismissRequest = { showDropdown = false }) {
+                startingPoint.utilityElement.assetType.terminalConfiguration?.terminals?.forEachIndexed { index, utilityTerminal ->
+                    DropdownMenuItem(
+                        text = {
+                            ReadOnlyTextField(
+                                text = utilityTerminal.name, leadingIcon = if (utilityTerminal.name == selectedConfigName) {
+                                    {
+                                        Icon(
+                                            imageVector = Icons.Filled.Done,
+                                            contentDescription = "Done icon",
+                                            modifier = Modifier.size(FilterChipDefaults.IconSize)
+                                        )
+                                    }
+                                } else {
+                                    null
+                                }
+                            )
+                        },
+                        onClick = {
+                            onTraceSelected(index)
+                            showDropdown = false
+                        },
+                        contentPadding = PaddingValues(vertical = 0.dp, horizontal = 10.dp)
+                    )
+                }
+            }
+        }
     }
 }
