729cddd13b03b60a4020613729f4f800bac07051
Puneet Prakash <puneet_prakash@esri.com>
2024-10-15 16:26:25 -0700

    add copyright

--- toolkit/utilitynetworks/src/main/java/com/arcgismaps/toolkit/utilitynetworks/ui/FeatureResultsDetailsScreen.kt
+++ toolkit/utilitynetworks/src/main/java/com/arcgismaps/toolkit/utilitynetworks/ui/FeatureResultsDetailsScreen.kt
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 2024 Esri
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.arcgismaps.toolkit.utilitynetworks.ui
 
 import androidx.compose.foundation.clickable
@@ -28,6 +43,11 @@ import com.arcgismaps.toolkit.utilitynetworks.internal.util.TabRow
 import com.arcgismaps.toolkit.utilitynetworks.internal.util.UpButton
 import com.arcgismaps.utilitynetworks.UtilityElement
 
+/**
+ * Composable that displays the selected groups of feature results.
+ *
+ * @since 200.6.0
+ */
 @Composable
 internal fun FeatureResultsDetailsScreen(
     selectedGroupName: String,
