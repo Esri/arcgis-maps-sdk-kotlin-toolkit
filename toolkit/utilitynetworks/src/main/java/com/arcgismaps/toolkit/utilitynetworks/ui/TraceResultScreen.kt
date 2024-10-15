9b78c952700d6d053c8021f54ae903f797572b3d
Puneet Prakash <puneet_prakash@esri.com>
2024-10-15 12:05:10 -0700

    Starting point details screen (#604)
    * WIP
    
    * WIP
    
    * add changes to support zoomto, delete and to select terminal
    
    * add newline
    
    * address code review comments
    
    * add padding
    
    * WIP
    
    * address Rama's comments
    
    * address Erick's comments.
    
    * few formatting updates
    
    * fix to have the icon always visible

--- toolkit/utilitynetworks/src/main/java/com/arcgismaps/toolkit/utilitynetworks/ui/TraceResultScreen.kt
+++ toolkit/utilitynetworks/src/main/java/com/arcgismaps/toolkit/utilitynetworks/ui/TraceResultScreen.kt
@@ -19,7 +19,6 @@ package com.arcgismaps.toolkit.utilitynetworks.ui
 import android.annotation.SuppressLint
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -27,26 +26,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBackIos
 import androidx.compose.material.icons.automirrored.filled.ArrowForwardIos
-import androidx.compose.material.icons.outlined.Clear
-import androidx.compose.material.icons.outlined.LocationOn
-import androidx.compose.material.icons.outlined.MoreVert
 import androidx.compose.material3.Button
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -56,6 +45,7 @@ import com.arcgismaps.toolkit.ui.expandablecard.ExpandableCard
 import com.arcgismaps.toolkit.utilitynetworks.R
 import com.arcgismaps.toolkit.utilitynetworks.TraceRun
 import com.arcgismaps.toolkit.utilitynetworks.internal.util.TabRow
+import com.arcgismaps.toolkit.utilitynetworks.internal.util.Title
 import com.arcgismaps.utilitynetworks.UtilityElement
 import com.arcgismaps.utilitynetworks.UtilityTraceFunctionOutput
 
@@ -95,10 +85,10 @@ internal fun TraceResultScreen(
                 )
             }
 
-            TraceTitle(
+            Title(
                 selectedTraceRun.name,
-                onZoomToResults = onZoomToResults,
-                onDeleteResult = onDeleteResult
+                onZoomTo = onZoomToResults,
+                onDelete = onDeleteResult
             )
             LazyColumn {
                 item {
@@ -147,51 +137,6 @@ private fun TraceResultPager(
 
 private fun getTraceCounterString(currentTraceResult: Int, totalTraceResults: Int): String {
     return "Trace $currentTraceResult of $totalTraceResults"
-}
-
-@Composable
-private fun TraceTitle(traceName: String, onZoomToResults: () -> Unit, onDeleteResult: () -> Unit) {
-    var expanded by remember { mutableStateOf(false) }
-    Row(
-        modifier = Modifier
-            .fillMaxWidth(),
-        horizontalArrangement = Arrangement.Center
-    ) {
-        Box {
-            Row(modifier = Modifier.clickable {
-                expanded = !expanded
-            }, verticalAlignment = Alignment.CenterVertically) {
-                Text(
-                    modifier = Modifier.padding(end = 8.dp),
-                    text = traceName,
-                    style = MaterialTheme.typography.headlineMedium
-                )
-                Icon(
-                    imageVector = Icons.Outlined.MoreVert,
-                    contentDescription = ""
-                )
-            }
-            MaterialTheme(shapes = MaterialTheme.shapes.copy(extraSmall = RoundedCornerShape(16.dp))) {
-                DropdownMenu(
-                    modifier = Modifier.padding(start = 8.dp, end = 16.dp),
-                    expanded = expanded,
-                    onDismissRequest = {
-                        expanded = false
-                    }) {
-                    DropdownMenuItem(
-                        text = { Text(stringResource(R.string.zoom_to_result)) },
-                        onClick = onZoomToResults,
-                        leadingIcon = { Icon(Icons.Outlined.LocationOn, contentDescription = stringResource(R.string.zoom_to_trace_result)) }
-                    )
-                    DropdownMenuItem(
-                        text = { Text(stringResource(R.string.delete)) },
-                        onClick = onDeleteResult,
-                        leadingIcon = { Icon(Icons.Outlined.Clear, contentDescription = stringResource(R.string.delete_trace_result)) }
-                    )
-                }
-            }
-        }
-    }
 }
 
 @Composable
