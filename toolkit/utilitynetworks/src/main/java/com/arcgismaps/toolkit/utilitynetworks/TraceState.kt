5565a076aeb77a8b9650f637834e18ee814418de
Puneet Prakash <puneet_prakash@esri.com>
2024-10-30 16:44:33 -0700

    Update Result screen UI when the First element in the list is getting deleted (#628)
    * Update selected state for trace result graphics when 0th element gets deleted
    
    * update delete logic
    
    * address code review comment

--- toolkit/utilitynetworks/src/main/java/com/arcgismaps/toolkit/utilitynetworks/TraceState.kt
+++ toolkit/utilitynetworks/src/main/java/com/arcgismaps/toolkit/utilitynetworks/TraceState.kt
@@ -665,8 +665,10 @@ public class TraceState(
         selectedTrace.geometryResultsGraphics.forEach { graphicsOverlay.graphics.remove(it) }
         selectedTrace.startingPoints.forEach { it.graphic.isSelected = false }
         _completedTraces.removeAt(_selectedCompletedTraceIndex.value)
-        if (_selectedCompletedTraceIndex.value - 1 >= 0) {
-            _selectedCompletedTraceIndex.value -= 1
+        if (_selectedCompletedTraceIndex.value > 0 || (_selectedCompletedTraceIndex.value == 0 && _completedTraces.isNotEmpty())) {
+            if (_selectedCompletedTraceIndex.value > 0) {
+                _selectedCompletedTraceIndex.value -= 1
+            }
             updateSelectedStateForTraceResultsGraphics(_selectedCompletedTraceIndex.value, true)
         }
     }
