fa78f277c3572f70b18d4b071961d9474eff319a
Gunther Heppner <gheppner@esri.com>
2024-11-12 10:30:56 +0100

    cache _isManualRenderingEnabled and sync with SceneView when set

--- toolkit/geoview-compose/src/main/java/com/arcgismaps/toolkit/geoviewcompose/SceneViewProxy.kt
+++ toolkit/geoview-compose/src/main/java/com/arcgismaps/toolkit/geoviewcompose/SceneViewProxy.kt
@@ -41,6 +41,9 @@ import kotlin.time.DurationUnit
  */
 @Stable
 public class SceneViewProxy : GeoViewProxy("SceneView") {
+
+    private var _isManualRenderingEnabled = false
+
     /**
      * The view-based [com.arcgismaps.mapping.view.SceneView] that this SceneViewProxy will operate on. This should
      * be initialized by the composable [SceneView] when it enters the composition and set to null when
@@ -62,6 +65,11 @@ public class SceneViewProxy : GeoViewProxy("SceneView") {
      */
     internal fun setSceneView(sceneView: com.arcgismaps.mapping.view.SceneView?) {
         this.sceneView = sceneView
+        sceneView?.apply {
+            // sync up the manual rendering state as it may have been set before the SceneView was
+            // initialized
+            setManualRenderingEnabled(_isManualRenderingEnabled)
+        }
     }
 
     private val nullSceneViewErrorMessage: String =
@@ -155,8 +163,8 @@ public class SceneViewProxy : GeoViewProxy("SceneView") {
      * @see setManualRenderingEnabled
      * @since 200.4.0
      */
-    public val isManualRenderingEnabled: Boolean?
-        get() = sceneView?.isManualRenderingEnabled
+    public val isManualRenderingEnabled: Boolean
+        get() = _isManualRenderingEnabled
 
     /**
      * Sets whether manual rendering is enabled or not.
@@ -171,6 +179,7 @@ public class SceneViewProxy : GeoViewProxy("SceneView") {
      * @since 200.4.0
      */
     public fun setManualRenderingEnabled(isManualRenderingEnabled: Boolean) {
+        _isManualRenderingEnabled = isManualRenderingEnabled
         sceneView?.isManualRenderingEnabled = isManualRenderingEnabled
     }
 
