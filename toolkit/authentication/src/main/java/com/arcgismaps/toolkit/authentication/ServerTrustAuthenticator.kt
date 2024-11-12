1fbab13083552a803f5a0c29e0064ff8ad6f63b9
Erick Lopez Solis <erick_solis@esri.com>
2024-11-12 12:15:58 -0800

    Adds ServerTrust and UsernamePassword Authenticators UI (#659)

--- toolkit/authentication/src/main/java/com/arcgismaps/toolkit/authentication/ServerTrustAuthenticator.kt
+++ toolkit/authentication/src/main/java/com/arcgismaps/toolkit/authentication/ServerTrustAuthenticator.kt
@@ -18,14 +18,65 @@
 
 package com.arcgismaps.toolkit.authentication
 
-import androidx.compose.material3.AlertDialog
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import com.arcgismaps.httpcore.authentication.NetworkAuthenticationChallenge
+import com.arcgismaps.httpcore.authentication.NetworkAuthenticationType
+
+/**
+ * Displays a trust or distrust server prompt to the user in a Dialog.
+ *
+ * @param serverTrustChallenge the pending [ServerTrustChallenge] that initiated this prompt.
+ * @param modifier the [Modifier] to be applied to this ServerTrustAuthenticator.
+ * @since 200.6.0
+ */
+@Composable
+internal fun ServerTrustAuthenticatorDialog(
+    serverTrustChallenge: ServerTrustChallenge,
+    modifier: Modifier = Modifier
+) {
+    Dialog(
+        onDismissRequest = {
+            serverTrustChallenge.distrust()
+        }
+    ) {
+        Card(
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.surface,
+            ),
+            shape = RoundedCornerShape(16.dp)
+        ) {
+            ServerTrustAuthenticatorImpl(
+                serverTrustChallenge.challenge.hostname,
+                modifier,
+                onConfirm = { serverTrustChallenge.trust() },
+                onCancel = { serverTrustChallenge.distrust() }
+            )
+        }
+    }
+}
 
 /**
  * Displays a trust or distrust server prompt to the user.
@@ -39,28 +90,121 @@ internal fun ServerTrustAuthenticator(
     serverTrustChallenge: ServerTrustChallenge,
     modifier: Modifier = Modifier
 ) {
-    AlertDialog(
-        modifier = modifier,
-        text = {
-            val hostname = serverTrustChallenge.challenge.hostname
-            Text(
-                text = stringResource(id = R.string.server_trust_message, hostname),
-                style = MaterialTheme.typography.titleSmall,
-                textAlign = TextAlign.Start
-            )
-        },
-        onDismissRequest = { serverTrustChallenge.distrust() },
-        confirmButton = {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        ServerTrustAuthenticatorImpl(
+            serverTrustChallenge.challenge.hostname,
+            modifier,
+            onConfirm = { serverTrustChallenge.trust() },
+            onCancel = { serverTrustChallenge.distrust() }
+        )
+    }
+}
+
+/**
+ * Displays a trust or distrust server prompt to the user.
+ *
+ * @param hostname the hostname of the server to trust.
+ * @param modifier the [Modifier] to be applied to this ServerTrustAuthenticator.
+ * @param onConfirm the callback to be invoked when the user confirms the trust.
+ * @param onCancel the callback to be invoked when the user cancels the trust.
+ * @since 200.6.0
+ */
+@Composable
+private fun ServerTrustAuthenticatorImpl(
+    hostname: String,
+    modifier: Modifier = Modifier,
+    onConfirm: () -> Unit,
+    onCancel: () -> Unit,
+) {
+    Column(
+        modifier = modifier
+            .padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = stringResource(id = R.string.server_trust_message, hostname),
+            style = MaterialTheme.typography.bodyLarge,
+            textAlign = TextAlign.Start,
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.End,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
             TextButton(
-                onClick = { serverTrustChallenge.trust() }) {
-                Text(text = stringResource(id = R.string.allow_connection))
+                onClick = { onCancel() }
+            ) {
+                Text(
+                    text = stringResource(id = R.string.cancel),
+                )
             }
-        },
-        dismissButton = {
-            TextButton(
-                onClick = { serverTrustChallenge.distrust() }) {
-                Text(stringResource(id = R.string.cancel))
+            Spacer(modifier = Modifier.width(8.dp))
+            TextButton(onClick = { onConfirm() }) {
+                Text(
+                    text = stringResource(id = R.string.allow_connection),
+                    color = MaterialTheme.colorScheme.error,
+                    textAlign = TextAlign.End
+                )
             }
         }
+    }
+}
+
+@Preview
+@Composable
+private fun ServerTrustAuthenticatorImplPreview() {
+    ServerTrustAuthenticatorImpl(
+        "https://www.arcgis.com",
+        onConfirm = {},
+        onCancel = {}
     )
 }
+
+@Preview
+@Composable
+private fun ServerTrustAuthenticatorPreview() {
+    val modifier = Modifier
+    ServerTrustAuthenticator(
+        serverTrustChallenge = ServerTrustChallenge(
+            NetworkAuthenticationChallenge(
+                "https://www.arcgis.com",
+                NetworkAuthenticationType.ServerTrust,
+                Throwable("Untrusted Host")
+            )
+        ) {},
+        modifier = modifier
+    )
+}
+
+@Preview
+@Composable
+private fun ServerTrustAuthenticatorDialogPreview() {
+    val modifier = Modifier
+    ServerTrustAuthenticatorDialog(
+        serverTrustChallenge = ServerTrustChallenge(
+            NetworkAuthenticationChallenge(
+                "https://www.arcgis.com",
+                NetworkAuthenticationType.ServerTrust,
+                Throwable("Untrusted Host")
+            )
+        ) {},
+        modifier = modifier
+    )
+}
+
+@Preview(locale = "de") // Preview in German for long translations
+@Composable
+private fun ServerTrustAuthenticatorDialogLocalePreview() {
+    ServerTrustAuthenticatorDialog(
+        serverTrustChallenge = ServerTrustChallenge(
+            NetworkAuthenticationChallenge(
+                "https://www.arcgis.com",
+                NetworkAuthenticationType.ServerTrust,
+                Throwable("Untrusted Host")
+            )
+        ) {}
+    )
+}
+
--- toolkit/authentication/src/main/java/com/arcgismaps/toolkit/authentication/UsernamePasswordAuthenticator.kt
+++ toolkit/authentication/src/main/java/com/arcgismaps/toolkit/authentication/UsernamePasswordAuthenticator.kt
@@ -18,8 +18,50 @@
 
 package com.arcgismaps.toolkit.authentication
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyEventType
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onPreviewKeyEvent
+import androidx.compose.ui.input.key.type
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 
 /**
  * Displays a username and password prompt to the user.
@@ -28,13 +70,235 @@ import androidx.compose.ui.Modifier
  * @param modifier the [Modifier] to be applied to this UsernamePasswordAuthenticator.
  * @since 200.2.0
  */
+@Deprecated(
+    message = "This function will be removed with the next major version change of the toolkit and should not be used directly." +
+            "The Authenticator composable displays UsernamePasswordAuthenticator automatically.",
+    level = DeprecationLevel.WARNING
+)
 @Composable
 public fun UsernamePasswordAuthenticator(
     usernamePasswordChallenge: UsernamePasswordChallenge,
     modifier: Modifier = Modifier
 ) {
-    UsernamePasswordAuthenticatorImpl(
-        usernamePasswordChallenge = usernamePasswordChallenge,
+    val additionalInfo = usernamePasswordChallenge.additionalMessage.collectAsStateWithLifecycle().value
+    Surface(modifier = Modifier.fillMaxSize()) {
+        UsernamePasswordAuthenticatorImpl(
+            hostname = usernamePasswordChallenge.hostname,
+            additionalInfo = additionalInfo ?: "",
+            onCancel = { usernamePasswordChallenge.cancel() },
+            onConfirm = { username, password ->
+                usernamePasswordChallenge.continueWithCredentials(username, password)
+            },
+            modifier = modifier
+        )
+    }
+}
+
+/**
+ * Displays a username and password prompt to the user in a dialog.
+ *
+ * @param usernamePasswordChallenge the pending [UsernamePasswordChallenge] that initiated this prompt.
+ * @param modifier the [Modifier] to be applied to this UsernamePasswordAuthenticator.
+ * @since 200.2.0
+ */
+@Composable
+internal fun UsernamePasswordAuthenticatorDialog(
+    usernamePasswordChallenge: UsernamePasswordChallenge,
+    modifier: Modifier = Modifier
+) {
+    val additionalInfo = usernamePasswordChallenge.additionalMessage.collectAsStateWithLifecycle().value
+
+    Dialog(onDismissRequest = { usernamePasswordChallenge.cancel() }) {
+        Card(
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.surface
+            ),
+            shape = RoundedCornerShape(16.dp)
+        ) {
+            UsernamePasswordAuthenticatorImpl(
+                hostname = usernamePasswordChallenge.hostname,
+                additionalInfo = additionalInfo ?: "",
+                onConfirm = { username, password ->
+                    usernamePasswordChallenge.continueWithCredentials(username, password)
+                },
+                onCancel = { usernamePasswordChallenge.cancel() },
+                modifier = modifier
+            )
+        }
+    }
+}
+
+@Composable
+private fun UsernamePasswordAuthenticatorImpl(
+    hostname: String,
+    modifier: Modifier = Modifier,
+    additionalInfo: String = "",
+    onConfirm: (username: String, password: String) -> Unit,
+    onCancel: () -> Unit
+) {
+    var usernameFieldText by rememberSaveable { mutableStateOf("") }
+    var passwordFieldText by rememberSaveable { mutableStateOf("") }
+
+    fun submitUsernamePassword() {
+        if (usernameFieldText.isNotEmpty() && passwordFieldText.isNotEmpty()) {
+            onConfirm(usernameFieldText, passwordFieldText)
+            passwordFieldText = ""
+        }
+    }
+
+    val keyboardActions = remember {
+        KeyboardActions(
+            onSend = { submitUsernamePassword() }
+        )
+    }
+    val focusManager = LocalFocusManager.current
+
+    Column(
         modifier = modifier
+            .padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        if (additionalInfo.isNotEmpty()) {
+            Text(
+                text = additionalInfo,
+                style = MaterialTheme.typography.labelLarge,
+                color = MaterialTheme.colorScheme.error,
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+
+        Text(
+            text = stringResource(id = R.string.username_password_login_message, hostname),
+            style = MaterialTheme.typography.bodyLarge,
+            textAlign = TextAlign.Start
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .moveFocusOnTabEvent(focusManager) { submitUsernamePassword() },
+                value = usernameFieldText,
+                onValueChange = { it: String -> usernameFieldText = it },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Email,
+                    imeAction = ImeAction.Next
+                ),
+                label = { Text(text = stringResource(id = R.string.username_label)) },
+                singleLine = true
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .moveFocusOnTabEvent(focusManager) { submitUsernamePassword() },
+                value = passwordFieldText,
+                onValueChange = { it: String -> passwordFieldText = it },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password,
+                    imeAction = ImeAction.Send
+                ),
+                keyboardActions = keyboardActions,
+                label = { Text(text = stringResource(id = R.string.password_label)) },
+                singleLine = true,
+                visualTransformation = PasswordVisualTransformation(),
+            )
+        }
+        Spacer(modifier = Modifier.height(24.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.End,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            TextButton(
+                onClick = { onCancel() }
+            ) {
+                Text(stringResource(id = R.string.cancel))
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            TextButton(
+                enabled = usernameFieldText.isNotEmpty() && passwordFieldText.isNotEmpty(),
+                onClick = { submitUsernamePassword() }
+            ) {
+                Text(stringResource(id = R.string.login))
+            }
+        }
+    }
+}
+
+private fun Modifier.moveFocusOnTabEvent(focusManager: FocusManager, onEnter: () -> Unit) =
+    onPreviewKeyEvent {
+        if (it.type == KeyEventType.KeyDown) {
+            when (it.key.keyCode) {
+                Key.Tab.keyCode -> {
+                    focusManager.moveFocus(FocusDirection.Down); true
+                }
+
+                Key.Enter.keyCode -> {
+                    onEnter()
+                    true
+                }
+
+                else -> false
+            }
+        } else false
+    }
+
+
+@Preview
+@Composable
+private fun UsernamePasswordAuthenticatorImplPreview() {
+    val modifier = Modifier
+    UsernamePasswordAuthenticatorImpl(
+        hostname = "https://www.arcgis.com/",
+        additionalInfo = "Invalid username or password.",
+        onConfirm = { _, _ -> },
+        onCancel = {},
+        modifier = modifier
+    )
+}
+
+@Preview
+@Composable
+private fun UsernamePasswordAuthenticatorPreview() {
+    val modifier = Modifier
+    UsernamePasswordAuthenticator(
+        usernamePasswordChallenge = UsernamePasswordChallenge(
+            url = "https://www.arcgis.com/",
+            onUsernamePasswordReceived = { _, _ -> },
+            onCancel = {}
+        ),
+        modifier = modifier
+    )
+}
+
+@Preview
+@Composable
+private fun UsernamePasswordAuthenticatorDialogPreview() {
+    val modifier = Modifier
+    UsernamePasswordAuthenticatorDialog(
+        usernamePasswordChallenge = UsernamePasswordChallenge(
+            url = "https://www.arcgis.com/",
+            onUsernamePasswordReceived = { _, _ -> },
+            onCancel = {}
+        ),
+        modifier = modifier
+    )
+}
+
+@Preview(locale = "de")
+@Composable
+private fun UsernamePasswordAuthenticatorDialogLocalePreview() {
+    UsernamePasswordAuthenticatorDialog(
+        usernamePasswordChallenge = UsernamePasswordChallenge(
+            url = "https://www.arcgis.com/",
+            onUsernamePasswordReceived = { _, _ -> },
+            onCancel = {}
+        )
     )
 }
