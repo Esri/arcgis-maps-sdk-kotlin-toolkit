9972e98c6bf55310debbf108cbe9084b079e678e
Soren Roth <sor10874@esri.com>
2024-11-20 14:22:54 -0800

    wip

--- toolkit/indoors/build.gradle.kts
+++ toolkit/indoors/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
     id("artifact-deploy")
+    alias(libs.plugins.binary.compatibility.validator) apply true
 }
 
 android {
