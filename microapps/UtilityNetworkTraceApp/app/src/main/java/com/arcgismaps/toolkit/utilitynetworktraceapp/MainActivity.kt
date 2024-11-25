480271db15bc4b33b1de2fb1899e93b77dedd4fd
Puneet Prakash <puneet_prakash@esri.com>
2024-11-25 09:45:07 -0800

    refactor app to not crash and log an error

--- microapps/UtilityNetworkTraceApp/app/src/main/java/com/arcgismaps/toolkit/utilitynetworktraceapp/MainActivity.kt
+++ microapps/UtilityNetworkTraceApp/app/src/main/java/com/arcgismaps/toolkit/utilitynetworktraceapp/MainActivity.kt
@@ -19,17 +19,22 @@
 package com.arcgismaps.toolkit.utilitynetworktraceapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -41,6 +46,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.arcgismaps.ArcGISEnvironment
 import com.arcgismaps.httpcore.authentication.TokenCredential
@@ -66,15 +72,19 @@ fun UtilityNetworkTraceApp(traceViewModel: TraceViewModel) {
     var initialized by remember { mutableStateOf(false) }
 
     LaunchedEffect(Unit) {
-        val tokenCred =
-            TokenCredential.create(
-                "https://sampleserver7.arcgisonline.com/portal/sharing/rest",
-                username = BuildConfig.traceToolUser,
-                password = BuildConfig.traceToolPassword
-            ).getOrThrow()
+        try {
+            val tokenCred =
+                TokenCredential.create(
+                    "https://sampleserver7.arcgisonline.com/portal/sharing/rest",
+                    username = BuildConfig.traceToolUser,
+                    password = BuildConfig.traceToolPassword
+                ).getOrThrow()
 
-        ArcGISEnvironment.authenticationManager.arcGISCredentialStore.add(tokenCred)
-        initialized = true
+            ArcGISEnvironment.authenticationManager.arcGISCredentialStore.add(tokenCred)
+            initialized = true
+        } catch (e: Exception) {
+            Log.e("UtilityNetworkTraceApp", e.toString())
+        }
     }
     if (initialized) {
         Scaffold(
@@ -90,8 +100,15 @@ fun UtilityNetworkTraceApp(traceViewModel: TraceViewModel) {
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
-            CircularProgressIndicator(
-                modifier = Modifier.size(32.dp)
+            Icon(
+                Icons.Default.Info,
+                contentDescription = stringResource(id = R.string.error),
+                tint = MaterialTheme.colorScheme.error
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+            Text(
+                text = stringResource(id = R.string.failed_to_create_token_credential),
+                color = MaterialTheme.colorScheme.error
             )
         }
     }
