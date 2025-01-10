63588fda83336630ef3eba3c8f535521477e2ccb
Hudson Miears <hud10837@esri.com>
2025-01-10 11:25:11 +0200

    set application context automatically

--- toolkit/ar/src/main/java/com/arcgismaps/toolkit/ar/WorldScaleSceneView.kt
+++ toolkit/ar/src/main/java/com/arcgismaps/toolkit/ar/WorldScaleSceneView.kt
@@ -33,6 +33,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.arcgismaps.ArcGISEnvironment
 import com.arcgismaps.geometry.SpatialReference
 import com.arcgismaps.location.LocationDataSource
 import com.arcgismaps.location.LocationDataSourceStatus
@@ -218,6 +219,9 @@ public fun WorldScaleSceneView(
         }
     }
     DisposableEffect(locationDataSource) {
+        if (ArcGISEnvironment.applicationContext == null) {
+            ArcGISEnvironment.applicationContext = context.applicationContext
+        }
         coroutineScope.launch {
             locationDataSource.start()
         }
