522f62b745cc62da18e54794ae219fdaafff1880
Erick <Erick_Solis@esri.com>
2025-01-29 14:55:23 -0800

    remove params

--- toolkit/scalebar/src/main/java/com/arcgismaps/toolkit/scalebar/internal/ScalebarDivision.kt
+++ toolkit/scalebar/src/main/java/com/arcgismaps/toolkit/scalebar/internal/ScalebarDivision.kt
@@ -3,9 +3,7 @@ package com.arcgismaps.toolkit.scalebar.internal
 /**
  * Represents a Scalebar division.
  *
- * @param index The index of the division.
  * @param xOffset The x offset of the division.
- * @param labelYOffset The y offset of the division's label from the scalebar.
  * @param label The text of the division's label.
  *
  * @since 200.7.0
