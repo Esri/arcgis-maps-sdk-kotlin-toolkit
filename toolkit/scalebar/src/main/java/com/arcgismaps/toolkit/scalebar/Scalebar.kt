c8a6358297679248b834e4a13460eb7603553aca
Puneet Prakash <puneet_prakash@esri.com>
2025-02-03 12:57:55 -0800

    Add Changes for DualUnitLine (#736)
    * Add Changes for DualUnitLine
    
    * add newlines
    
    * update create divisions logic
    
    * update preview

--- toolkit/scalebar/src/main/java/com/arcgismaps/toolkit/scalebar/Scalebar.kt
+++ toolkit/scalebar/src/main/java/com/arcgismaps/toolkit/scalebar/Scalebar.kt
@@ -43,6 +43,7 @@ import com.arcgismaps.geometry.SpatialReference
 import com.arcgismaps.mapping.Viewpoint
 import com.arcgismaps.toolkit.scalebar.internal.AlternatingBarScalebar
 import com.arcgismaps.toolkit.scalebar.internal.BarScalebar
+import com.arcgismaps.toolkit.scalebar.internal.DualUnitLineScalebar
 import com.arcgismaps.toolkit.scalebar.internal.GraduatedLineScalebar
 import com.arcgismaps.toolkit.scalebar.internal.LineScalebar
 import com.arcgismaps.toolkit.scalebar.internal.ScalebarDivision
@@ -128,7 +129,8 @@ public fun Scalebar(
     // update the label text and offsets
     val scalebarDivisions = scalebarProperties.computeDivisions(
         minSegmentWidth = minSegmentWidth,
-        scalebarStyle = style
+        scalebarStyle = style,
+        units = units
     )
 
     AnimatedVisibility(
@@ -185,7 +187,16 @@ private fun Scalebar(
             shapes = shapes
         )
 
-        ScalebarStyle.DualUnitLine -> TODO()
+        ScalebarStyle.DualUnitLine -> DualUnitLineScalebar(
+            modifier = modifier,
+            maxWidth = maxWidth,
+            primaryScalebarDivision = labels.first(),
+            alternateScalebarDivision = labels.last(),
+            colorScheme = colorScheme,
+            labelTypography = labelTypography,
+            shapes = shapes
+        )
+
         ScalebarStyle.GraduatedLine -> GraduatedLineScalebar(
             modifier = modifier,
             maxWidth = maxWidth,
