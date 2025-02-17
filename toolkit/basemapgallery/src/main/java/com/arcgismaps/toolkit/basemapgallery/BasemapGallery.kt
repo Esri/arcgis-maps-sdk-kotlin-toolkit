9511a7493bb20a3433c7e0312e9c049236fd93bc
Colin Anderson <colin_anderson@esri.com>
2025-02-17 13:48:34 +0000

    Remove unused import

--- toolkit/basemapgallery/src/main/java/com/arcgismaps/toolkit/basemapgallery/BasemapGallery.kt
+++ toolkit/basemapgallery/src/main/java/com/arcgismaps/toolkit/basemapgallery/BasemapGallery.kt
@@ -29,7 +29,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
