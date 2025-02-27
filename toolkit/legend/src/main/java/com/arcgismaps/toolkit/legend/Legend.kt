5917b1dbaee6b81477ce1d2c6e109537ae56b7eb
Puneet Prakash <puneet_prakash@esri.com>
2025-02-26 17:09:32 -0800

    Remove Coil Add logic cache Symbols and their bitmapDrawables

--- toolkit/legend/src/main/java/com/arcgismaps/toolkit/legend/Legend.kt
+++ toolkit/legend/src/main/java/com/arcgismaps/toolkit/legend/Legend.kt
@@ -21,6 +21,7 @@ package com.arcgismaps.toolkit.legend
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -34,27 +35,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
-import coil.ImageLoader
-import coil.compose.AsyncImage
-import coil.decode.DataSource
-import coil.decode.ImageSource
-import coil.disk.DiskCache
-import coil.fetch.FetchResult
-import coil.fetch.Fetcher
-import coil.fetch.SourceResult
-import coil.memory.MemoryCache
-import coil.request.CachePolicy
-import coil.request.ImageRequest
-import coil.request.Options
-import coil.util.DebugLogger
 import com.arcgismaps.mapping.GeoModel
 import com.arcgismaps.mapping.layers.Layer
 import com.arcgismaps.mapping.layers.LayerContent
 import com.arcgismaps.mapping.layers.LegendInfo
 import com.arcgismaps.mapping.symbology.Symbol
-import okio.Buffer
-import java.io.ByteArrayOutputStream
 
 @Immutable
 internal data class LayerRow (
@@ -70,6 +57,7 @@ public fun Legend(
     modifier: Modifier = Modifier
 ) {
     var legendItems by rememberSaveable(geoModel) { mutableStateOf(emptyList<LayerRow>()) }
+    val symbolCache = rememberSaveable(geoModel) { mutableMapOf<Symbol, BitmapDrawable>() }
 
     if (legendItems.isEmpty()) {
         LaunchedEffect(geoModel) {
@@ -86,7 +74,12 @@ public fun Legend(
     }
 
     if (legendItems.isNotEmpty()) {
-        Legend(modifier, legendItems, currentScale)
+        Legend(
+            modifier,
+            legendItems,
+            currentScale,
+            { symbol, bitmapDrawable -> symbolCache[symbol] = bitmapDrawable },
+            { symbol -> symbolCache[symbol] })
     }
 }
 
@@ -95,6 +88,8 @@ private fun Legend(
     modifier: Modifier,
     legendItems: List<LayerRow>,
     currentScale: Double,
+    addToSymbolCache: (Symbol, BitmapDrawable) -> Unit,
+    getFromSymbolCache: (Symbol) -> BitmapDrawable?
     ) {
     LazyColumn(modifier = modifier) {
         items(legendItems) { item ->
@@ -104,7 +99,12 @@ private fun Legend(
                 }
                 if (item.legendInfos.isNotEmpty()) {
                     item.legendInfos.forEach { legendInfo ->
-                        LegendInfoRow(legendInfo, LocalContext.current)
+                        LegendInfoRow(
+                            legendInfo,
+                            LocalContext.current,
+                            addToSymbolCache,
+                            getFromSymbolCache
+                        )
                     }
                 }
             }
@@ -113,26 +113,33 @@ private fun Legend(
 }
 
 @Composable
-private fun LegendInfoRow(legendInfo: LegendInfo, context: Context) {
+private fun LegendInfoRow(
+    legendInfo: LegendInfo,
+    context: Context,
+    addToSymbolCache: (Symbol, BitmapDrawable) -> Unit,
+    getFromSymbolCache: (Symbol) -> BitmapDrawable?
+) {
     Row {
         val symbol = legendInfo.symbol
         if (symbol != null) {
-            val imageFetcher = remember { SymbolImageFetcher.Factory(symbol, context) }
-            val imageLoader = rememberImageLoader(context)
             val image = remember { mutableStateOf<Bitmap?>(null) }
 
             LaunchedEffect(legendInfo) {
-                val request = ImageRequest.Builder(context)
-                    .data(symbol)
-                    .fetcherFactory(imageFetcher)
-                    .build()
-                val result = imageLoader.execute(request)
-                image.value = (result.drawable as? BitmapDrawable)?.bitmap
+                val cachedSwatch = getFromSymbolCache(symbol)
+                if (cachedSwatch != null) {
+                    image.value = cachedSwatch.bitmap
+                } else {
+                    val swatch = symbol.createSwatch(context.resources.displayMetrics.density).getOrNull()
+                    swatch?.let {
+                        addToSymbolCache(symbol, it)
+                        image.value = it.bitmap
+                    }
+                }
             }
 
             image.value?.let {
-                AsyncImage(
-                    model = it,
+                Image(
+                    bitmap = it.asImageBitmap(),
                     contentDescription = null
                 )
             }
@@ -224,58 +231,3 @@ private suspend fun fetchLayerRowsWithSublayersAndLegendInfos(layerContent: Laye
     }
     return layerRows
 }
-
-@Composable
-private fun rememberImageLoader(context: Context): ImageLoader {
-    return remember {
-        ImageLoader(context).newBuilder()
-            .memoryCachePolicy(CachePolicy.ENABLED)
-            .memoryCache {
-                MemoryCache.Builder(context)
-                    .maxSizePercent(0.25)
-                    .strongReferencesEnabled(true)
-                    .build()
-            }
-            .diskCachePolicy(CachePolicy.ENABLED)
-            .diskCache {
-                DiskCache.Builder()
-                    .directory(context.cacheDir)
-                    .maxSizePercent(.03)
-                    .build()
-            }
-            .logger(DebugLogger())
-            .build()
-    }
-}
-
-internal class SymbolImageFetcher(
-    private val symbol: Symbol,
-    private val context: Context
-) : Fetcher {
-
-    override suspend fun fetch(): FetchResult {
-        val response =
-            symbol.createSwatch(context.resources.displayMetrics.density).getOrNull()?.bitmap
-
-        val bos = ByteArrayOutputStream()
-        val buffer = Buffer()
-        bos.use {
-            response?.compress(Bitmap.CompressFormat.PNG, 100, bos)
-            buffer.write(bos.toByteArray())
-        }
-        return SourceResult(
-            source = ImageSource(buffer, context),
-            dataSource = DataSource.MEMORY,
-            mimeType = null
-        )
-    }
-
-    class Factory(
-        private val symbol: Symbol,
-        private val context: Context
-    ) : Fetcher.Factory<Symbol> {
-        override fun create(data: Symbol, options: Options, imageLoader: ImageLoader): Fetcher =
-            SymbolImageFetcher(symbol, context)
-    }
-}
-
