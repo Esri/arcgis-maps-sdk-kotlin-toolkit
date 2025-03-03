1987cff82ccdf56f11635e27671c8ac573c4c6ad
Colin Anderson <colin_anderson@esri.com>
2025-03-03 18:11:58 +0000

    Feature branches/basemap gallery (#768)
    * Setting up classes etc
    
    * Micro app
    
    * Placemark image
    
    * Placemark image
    
    * Add image resource to use in preview
    
    * New API and way of handling the thumbnails
    
    * Don't need the receiver
    
    * Cleaning up
    
    * Add doc and remove app preview
    
    * Api file
    
    * Fix end of file
    
    * Fix end of file
    
    * Remove commented code
    
    * Consistent punctuation
    
    * Readme
    
    * Update README.md
    
    * Update README.md
    
    Add code
    
    * Colinanderson/basemap gallery bottom sheet (#742)
    
    * Use a bottom sheet for the basemap gallery
    
    * Correct copyright
    
    * Formatting
    
    * Blank line
    
    * Fix whitespace
    
    * Update README.md
    
    * Colinanderson/basemap gallery bottom sheet (#746)
    
    * Use a bottom sheet for the basemap gallery
    
    * Correct copyright
    
    * Formatting
    
    * Blank line
    
    * Fix whitespace
    
    * Align text
    
    * Rounded corners
    
    * Start expanded
    
    * UInused import
    
    * Colinanderson/basemap gallery bottom sheet (#747)
    
    * Use a bottom sheet for the basemap gallery
    
    * Correct copyright
    
    * Formatting
    
    * Blank line
    
    * Fix whitespace
    
    * Align text
    
    * Rounded corners
    
    * Start expanded
    
    * UInused import
    
    * Add support for portal items
    
    * Unused imports
    
    * Update README.md
    
    * Code review updates
    
    * Code review updates
    
    * Code review update
    
    * Log warnings for set up failures
    
    * Add since and space out params
    
    * Code review updates
    
    * Code review update
    
    * Code review update
    
    * Colinanderson/basemap gallery bottom sheet (#756)
    
    * Use a bottom sheet for the basemap gallery
    
    * Correct copyright
    
    * Formatting
    
    * Blank line
    
    * Fix whitespace
    
    * Align text
    
    * Rounded corners
    
    * Start expanded
    
    * UInused import
    
    * Add support for portal items
    
    * Unused imports
    
    * Indicate which basemap is selected
    
    * Experiment with not using a card
    
    * Update copyright to 2025
    
    * Update screenshot
    
    * Update screenshot
    
    * Resize
    
    * Update README.md
    
    * Update screenshot
    
    * Fix typo
    
    * Update README.md
    
    * Remove log output
    
    * Reset the viewpoint when the basemap changes
    
    * Fix name clash bug
    
    * Compute scalebar properties everytime the minScale, useGeodeticCalculations and units also change (#753)
    
    * Remove commented code
    
    * Remove unused import
    
    * Fix merge mistake
    
    * Refactor
    
    * Switch to SVG for placeholder
    
    * Add basemap gallery to main readme
    
    * Fix blank lines
    
    * Scalebar: Autohide delay fix (#758)
    
    * Gallery wasn't updating the thumbnails when the items changed
    
    * Refactor
    
    * Code review updates
    
    * Remove comment that only made sense when considering the micro app
    
    * Fix typo
    
    * Add readme for app
    
    * Do not set initial viewpoint when changig basemap
    
    * Code review update
    
    * Code review update
    
    * Code review updates
    
    * Resize screen shot
    
    * Upgrade versions to fix vtest problem
    
    * Trying to fix vtest error
    
    * Update sdkVersionNumber (#686)
    
    * Update sdkVersionNumber, remove sdkBuildNumber property from gradle.properties
    Remove logic to pick up build number from gradle.properties
    add logic to pick up build number from local.properties
    
    * update doc
    
    * Update API def
    
    * Load local.properties file only when it exists, so we don't throw an exception when it does not exist (#763)
    
    * Don't need to update version
    
    * Updtae kotlinxSerializationJson version
    
    * Colinanderson/vtest problems (#764)
    
    * Update Kotlin version
    
    * Try to fix problem
    
    ---------
    
    Co-authored-by: Puneet Prakash <puneet_prakash@esri.com>
    Co-authored-by: Shubham Sharma <shubhamsharma@esri.com>
    
    * Code review updates
    
    * Fix merge
    
    * Fix merge
    
    * Fix merge
    
    * Code review update
    
    ---------
    
    Co-authored-by: Puneet Prakash <puneet_prakash@esri.com>
    Co-authored-by: Shubham Sharma <shubhamsharma@esri.com>

---
+++ microapps/BasemapGalleryApp/app/src/main/java/com/arcgismaps/toolkit/basemapgalleryapp/ViewModel.kt
@@ -0,0 +1,103 @@
+/*
+ *
+ *  Copyright 2025 Esri
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package com.arcgismaps.toolkit.basemapgalleryapp
+
+import android.app.Application
+import android.util.Log
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.arcgismaps.geometry.Point
+import com.arcgismaps.geometry.SpatialReference
+import com.arcgismaps.mapping.ArcGISMap
+import com.arcgismaps.mapping.Basemap
+import com.arcgismaps.mapping.BasemapStyle
+import com.arcgismaps.mapping.BasemapStylesService
+import com.arcgismaps.mapping.Viewpoint
+import com.arcgismaps.portal.Portal
+import com.arcgismaps.toolkit.basemapgallery.BasemapGalleryItem
+import kotlinx.coroutines.launch
+
+/**
+ * The view model.
+ *
+ * @param application the application associated with this view model
+ * @constructor constructs a viewmodel
+ *
+ * @since 200.7.0
+ */
+class ViewModel(application: Application) : AndroidViewModel(application) {
+
+    var styleItems = mutableStateListOf<BasemapGalleryItem>()
+        private set
+    var portalItems = mutableStateListOf<BasemapGalleryItem>()
+        private set
+
+    val arcGISMap =
+        ArcGISMap(BasemapStyle.ArcGISImagery).apply {
+            initialViewpoint =
+                Viewpoint(
+                    center = Point(-11e6, 5e6, SpatialReference.webMercator()),
+                    scale = 1e8
+                )
+        }
+
+    init {
+        viewModelScope.launch {
+            // get basemap portal items from a portal
+            val portal = Portal("https://www.arcgis.com")
+            portal.load()
+                .onFailure { Log.w("BasemapGallery", "Failed to load ${portal.url}") }
+                .onSuccess {
+                    portal.fetchBasemaps()
+                        .onFailure { Log.w("BasemapGallery", "Failed to fetch basemaps") }
+                        .onSuccess {
+                            it.forEach { basemap ->
+                                basemap.item?.let { item ->
+                                    val galleryItem = BasemapGalleryItem(item)
+                                    portalItems.add(galleryItem)
+                                }
+                            }
+                        }
+                }
+
+            // get basemap style info from a basemap style service
+            val service = BasemapStylesService()
+            service.load()
+                .onFailure { Log.w("BasemapGallery", "Failed to load basemap styles") }
+                .onSuccess {
+                    // for each basemap style info create a gallery item and add it to the list of items
+                    service.info?.stylesInfo?.forEach { basemapStyleInfo ->
+                        val galleryItem = BasemapGalleryItem(basemapStyleInfo)
+                        styleItems.add(galleryItem)
+                    }
+                }
+        }
+    }
+
+    /**
+     * Change the basemap and reset the viewpoint.
+     *
+     * @param basemap to set
+     * @since 200.7.0
+     */
+    fun changeBasemap(basemap: Basemap) {
+        arcGISMap.setBasemap(basemap)
+    }
+}
