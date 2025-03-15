88215266e545b9e8b9c30c87d8b3006f9a803e6a
Gunther Heppner <gheppner@esri.com>
2025-03-15 21:31:19 +0100

    add doc to PeDataTests

--- toolkit/ar/src/androidTest/java/com/arcgismaps/toolkit/ar/PeDataTests.kt
+++ toolkit/ar/src/androidTest/java/com/arcgismaps/toolkit/ar/PeDataTests.kt
@@ -1,3 +1,21 @@
+/*
+ *
+ *  Copyright 2025 Esri
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
 package com.arcgismaps.toolkit.ar
 
 import androidx.test.platform.app.InstrumentationRegistry
@@ -12,9 +30,14 @@ import org.junit.Before
 import org.junit.Test
 import java.io.File
 
+/**
+ * Tests for deploying PE data as part of [WorldScaleSceneView].
+ *
+ * @since 200.7.0
+ */
 class PeDataTests {
-    var initialPeDirectory: String? = null
-    val gridFile = File(
+    private var initialPeDirectory: String? = null
+    private val gridFile = File(
         InstrumentationRegistry.getInstrumentation().context.getExternalFilesDir(null),
         "$PEDATA_ROOT_DIRECTORY/$PEDATA_FILE_NAME"
     )
@@ -34,6 +57,16 @@ class PeDataTests {
         TransformationCatalog.projectionEngineDirectory = initialPeDirectory
     }
 
+    /**
+     * Given a PE data file that is part of the ar library assets,
+     * When [PeData.configure] is called,
+     * Then the PE data file should be copied to the external files directory.
+     *
+     * When [PeData.configure] is called again,
+     * Then the PE data file should not be copied again, but [PeData.configure] should return success.
+     *
+     * @since 200.7.0
+     */
     @Test
     fun configurePeData() = runTest {
         assertThat(TransformationCatalog.projectionEngineDirectory).isEmpty()
@@ -43,14 +76,25 @@ class PeDataTests {
         assertThat(PeData.configure(context).isSuccess).isTrue()
         assertThat(gridFile.exists()).isTrue()
         assertThat(gridFile.isFile).isTrue()
+        assertThat(TransformationCatalog.projectionEngineDirectory).isEqualTo(gridFile.parent)
 
         // call configure again, to check that it returns success also when PE data
         // have already been configured
         assertThat(PeData.configure(context).isSuccess).isTrue()
         assertThat(gridFile.exists()).isTrue()
         assertThat(gridFile.isFile).isTrue()
+        assertThat(TransformationCatalog.projectionEngineDirectory).isEqualTo(gridFile.parent)
     }
 
+    /**
+     * Given a PE data file that is part of the ar library assets,
+     * When [TransformationCatalog.projectionEngineDirectory] has been set prior to
+     * calling [PeData.configure],
+     * Then the PE data file is not copied from the assets and the path of
+     * [TransformationCatalog.projectionEngineDirectory] remains unchanged.
+     *
+     * @since 200.7.0
+     */
     @Test
     fun configurePeDataPathAlreadySet() = runTest {
         assertThat(TransformationCatalog.projectionEngineDirectory).isEmpty()
