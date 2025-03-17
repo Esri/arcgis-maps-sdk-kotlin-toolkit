baab8c11b46303b859bd9c3c4f77b4ad6743151c
Kaushik Meesala <kmeesala@esri.com>
2025-03-17 14:45:51 -0700

    Update ConfirmationDialogs.kt

--- toolkit/featureforms/src/main/java/com/arcgismaps/toolkit/featureforms/internal/components/dialogs/ConfirmationDialogs.kt
+++ toolkit/featureforms/src/main/java/com/arcgismaps/toolkit/featureforms/internal/components/dialogs/ConfirmationDialogs.kt
@@ -33,6 +33,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.window.DialogProperties
 import com.arcgismaps.toolkit.featureforms.R
 
 @Composable
@@ -69,6 +70,10 @@ internal fun SaveEditsDialog(
                 Text(stringResource(R.string.discard))
             }
         },
+        properties = DialogProperties(
+            dismissOnClickOutside = false,
+            dismissOnBackPress = false
+        )
     )
 }
 
@@ -90,7 +95,11 @@ internal fun ErrorDialog(
             TextButton(onClick = onDismissRequest) {
                 Text(stringResource(R.string.ok))
             }
-        }
+        },
+        properties = DialogProperties(
+            dismissOnClickOutside = false,
+            dismissOnBackPress = false
+        )
     )
 }
 
