d1d102731d80560caad073bd50e25b399c38666e
Soren Roth <sor10874@esri.com>
2025-03-25 12:59:56 -0700

    PR feedback

--- microapps/LegendApp/app/src/main/java/com/arcgismaps/toolkit/legendapp/screens/MainScreen.kt
+++ microapps/LegendApp/app/src/main/java/com/arcgismaps/toolkit/legendapp/screens/MainScreen.kt
@@ -34,7 +34,6 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableDoubleStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
