b0c55a1d3184370c4be57e1070ef41df37f3e488
Hudson Miears <hud10837@esri.com>
2025-03-28 16:16:03 +0200

    update test to test to string

--- toolkit/ar/src/androidTest/java/com/arcgismaps/toolkit/ar/WorldScaleTrackingModeTests.kt
+++ toolkit/ar/src/androidTest/java/com/arcgismaps/toolkit/ar/WorldScaleTrackingModeTests.kt
@@ -1,3 +1,21 @@
+/*
+ *
+ *  Copyright 2025 Esri
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
 package com.arcgismaps.toolkit.ar
 
 import com.google.common.truth.Truth.assertThat
@@ -35,5 +53,11 @@ class WorldScaleTrackingModeTests {
 
         assertThat(worldMode1).isNotEqualTo(geospatialMode1)
         assertThat(worldMode1.hashCode()).isNotEqualTo(geospatialMode1.hashCode())
+
+        assertThat(worldMode1.toString()).isEqualTo("World")
+        assertThat(worldMode2.toString()).isEqualTo(worldMode1.toString())
+
+        assertThat(geospatialMode1.toString()).isEqualTo("Geospatial")
+        assertThat(geospatialMode2.toString()).isEqualTo(geospatialMode1.toString())
     }
 }
