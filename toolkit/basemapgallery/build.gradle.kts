79d14881eb807fc4b0bba3ca23fb304449eb054b
Colin Anderson <colin_anderson@esri.com>
2025-04-02 15:39:44 +0100

    Comment out all the test stuff

--- toolkit/basemapgallery/build.gradle.kts
+++ toolkit/basemapgallery/build.gradle.kts
@@ -61,22 +61,22 @@ android {
         }
     }
 
-    /**
-     * Configures the test report for connected (instrumented) tests to be copied to a central
-     * folder in the project's root directory.
-     */
-    @Suppress("UnstableApiUsage")
-    testOptions {
-        targetSdk = libs.versions.compileSdk.get().toInt()
-        val connectedTestReportsPath: String by project
-        reportDir = "$connectedTestReportsPath/${project.name}"
-    }
+//    /**
+//     * Configures the test report for connected (instrumented) tests to be copied to a central
+//     * folder in the project's root directory.
+//     */
+//    @Suppress("UnstableApiUsage")
+//    testOptions {
+//        targetSdk = libs.versions.compileSdk.get().toInt()
+//        val connectedTestReportsPath: String by project
+//        reportDir = "$connectedTestReportsPath/${project.name}"
+//    }
 
     // Avoids an empty test report showing up in the CI integration test report.
     // Remove this if tests will be added.
-    tasks.withType<Test> {
-        enabled = false
-    }
+//    tasks.withType<Test> {
+//        enabled = false
+//    }
 }
 
 dependencies {
