5f01367c94a314167d389d4a2299531eb06cd0a5
Puneet Prakash <puneet_prakash@esri.com>
2025-04-21 15:32:03 -0700

    remove annotation for data classes whose public api will be affected

--- toolkit/legend/build.gradle.kts
+++ toolkit/legend/build.gradle.kts
@@ -46,8 +46,6 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
-        // This flag is the same as applying '@ConsistentCopyVisibility' annotation to all data classes in the module.
-        freeCompilerArgs = freeCompilerArgs + listOf("-Xconsistent-data-class-copy-visibility")
     }
     buildFeatures {
         compose = true
