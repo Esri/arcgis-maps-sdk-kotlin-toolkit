12a005d7c83260c3651c5fd4f026789658b3888f
Shubham Sharma <shubhamsharma@esri.com>
2025-04-23 12:38:34 -0700

    changed isMinifyEnabled -> false

--- microapps/OfflineMapAreasApp/app/build.gradle.kts
+++ microapps/OfflineMapAreasApp/app/build.gradle.kts
@@ -47,13 +47,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
-            isShrinkResources = true
-
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            isMinifyEnabled = false
         }
     }
     compileOptions {
