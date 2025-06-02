b985c9e9ee19a1a0d94abc3b686f3a4c0f835f9f
Colin Anderson <colin_anderson@esri.com>
2025-06-02 14:36:14 +0100

    Proxy

--- toolkit/ar/src/main/java/com/arcgismaps/toolkit/ar/FlyoverSceneView.kt
+++ toolkit/ar/src/main/java/com/arcgismaps/toolkit/ar/FlyoverSceneView.kt
@@ -19,6 +19,7 @@
 package com.arcgismaps.toolkit.ar
 
 import android.Manifest
+import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -27,20 +28,44 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import com.arcgismaps.geometry.Point
+import com.arcgismaps.geometry.SpatialReference
 import com.arcgismaps.mapping.ArcGISScene
+import com.arcgismaps.mapping.TimeExtent
+import com.arcgismaps.mapping.Viewpoint
+import com.arcgismaps.mapping.view.AnalysisOverlay
 import com.arcgismaps.mapping.view.AtmosphereEffect
+import com.arcgismaps.mapping.view.AttributionBarLayoutChangeEvent
 import com.arcgismaps.mapping.view.Camera
+import com.arcgismaps.mapping.view.DoubleTapEvent
+import com.arcgismaps.mapping.view.DownEvent
+import com.arcgismaps.mapping.view.GeoView
+import com.arcgismaps.mapping.view.GraphicsOverlay
+import com.arcgismaps.mapping.view.ImageOverlay
+import com.arcgismaps.mapping.view.LightingMode
+import com.arcgismaps.mapping.view.LongPressEvent
+import com.arcgismaps.mapping.view.PanChangeEvent
+import com.arcgismaps.mapping.view.RotationChangeEvent
+import com.arcgismaps.mapping.view.ScaleChangeEvent
+import com.arcgismaps.mapping.view.SceneViewInteractionOptions
+import com.arcgismaps.mapping.view.SelectionProperties
+import com.arcgismaps.mapping.view.SingleTapConfirmedEvent
 import com.arcgismaps.mapping.view.SpaceEffect
 import com.arcgismaps.mapping.view.TransformationMatrixCameraController
+import com.arcgismaps.mapping.view.TwoPointerTapEvent
+import com.arcgismaps.mapping.view.UpEvent
+import com.arcgismaps.mapping.view.ViewLabelProperties
 import com.arcgismaps.toolkit.ar.internal.ArCameraFeed
 import com.arcgismaps.toolkit.ar.internal.rememberArSessionWrapper
 import com.arcgismaps.toolkit.ar.internal.rememberPermissionsGranted
 import com.arcgismaps.toolkit.ar.internal.transformationMatrix
 import com.arcgismaps.toolkit.geoviewcompose.SceneView
-import com.arcgismaps.toolkit.geoviewcompose.SceneViewProxy
+import com.arcgismaps.toolkit.geoviewcompose.SceneViewDefaults
 import com.google.ar.core.Config
+import com.google.ar.core.TrackingState
+import java.time.Instant
 
 @Composable
 public fun FlyoverSceneView(
@@ -49,14 +74,46 @@ public fun FlyoverSceneView(
     initialHeading: Double,
     translationFactor: Double,
     modifier: Modifier = Modifier,
-    flyoverSceneViewProxy: FlyoverSceneViewProxy = remember { FlyoverSceneViewProxy() }
+    flyoverSceneViewProxy: FlyoverSceneViewProxy = remember { FlyoverSceneViewProxy() },
+    interactionOptions: SceneViewInteractionOptions = remember { SceneViewInteractionOptions() },
+    onViewpointChangedForCenterAndScale: ((Viewpoint) -> Unit)? = null,
+    onViewpointChangedForBoundingGeometry: ((Viewpoint) -> Unit)? = null,
+    graphicsOverlays: List<GraphicsOverlay> = remember { emptyList() },
+    viewLabelProperties: ViewLabelProperties = remember { ViewLabelProperties() },
+    selectionProperties: SelectionProperties = remember { SelectionProperties() },
+    isAttributionBarVisible: Boolean = true,
+    onAttributionTextChanged: ((String) -> Unit)? = null,
+    onAttributionBarLayoutChanged: ((AttributionBarLayoutChangeEvent) -> Unit)? = null,
+    analysisOverlays: List<AnalysisOverlay> = remember { emptyList() },
+    imageOverlays: List<ImageOverlay> = remember { emptyList() },
+    atmosphereEffect: AtmosphereEffect = AtmosphereEffect.Realistic,
+    timeExtent: TimeExtent? = null,
+    onTimeExtentChanged: ((TimeExtent?) -> Unit)? = null,
+    spaceEffect: SpaceEffect = SpaceEffect.Stars,
+    sunTime: Instant = SceneViewDefaults.DefaultSunTime,
+    sunLighting: LightingMode = LightingMode.NoLight,
+    ambientLightColor: Color = SceneViewDefaults.DefaultAmbientLightColor,
+    onNavigationChanged: ((isNavigating: Boolean) -> Unit)? = null,
+    onSpatialReferenceChanged: ((spatialReference: SpatialReference?) -> Unit)? = null,
+    onLayerViewStateChanged: ((GeoView.GeoViewLayerViewStateChanged) -> Unit)? = null,
+    onInteractingChanged: ((isInteracting: Boolean) -> Unit)? = null,
+    onCurrentViewpointCameraChanged: ((camera: Camera) -> Unit)? = null,
+    onRotate: ((RotationChangeEvent) -> Unit)? = null,
+    onScale: ((ScaleChangeEvent) -> Unit)? = null,
+    onUp: ((UpEvent) -> Unit)? = null,
+    onDown: ((DownEvent) -> Unit)? = null,
+    onSingleTapConfirmed: ((SingleTapConfirmedEvent) -> Unit)? = null,
+    onDoubleTap: ((DoubleTapEvent) -> Unit)? = null,
+    onLongPress: ((LongPressEvent) -> Unit)? = null,
+    onTwoPointerTap: ((TwoPointerTapEvent) -> Unit)? = null,
+    onPan: ((PanChangeEvent) -> Unit)? = null
 ) {
     val context = LocalContext.current
 
     val cameraPermissionGranted by rememberPermissionsGranted(listOf(Manifest.permission.CAMERA)) {
         // onNotGranted
 //        initializationStatus.update(
-//            TableTopSceneViewStatus.FailedToInitialize(
+//            TFlyoverSceneViewStatus.FailedToInitialize(
 //                IllegalStateException(
 //                    context.getString(R.string.camera_permission_not_granted)
 //                )
@@ -71,7 +128,7 @@ public fun FlyoverSceneView(
 //        val arCoreAvailability = checkArCoreAvailability(context)
 //        if (arCoreAvailability != ArCoreApk.Availability.SUPPORTED_INSTALLED) {
 //            initializationStatus.update(
-//                TableTopSceneViewStatus.FailedToInitialize(
+//                FlyoverSceneViewStatus.FailedToInitialize(
 //                    IllegalStateException(context.getString(R.string.arcore_not_installed_message))
 //                ),
 //                onInitializationStatusChanged
@@ -91,30 +148,35 @@ public fun FlyoverSceneView(
             planeFindingMode = Config.PlaneFindingMode.DISABLED
         )
 
-//    val sceneViewProxy = remember {
-//        SceneViewProxy()
-//    }
-
     val cameraController = remember {
-        TransformationMatrixCameraController().apply {
-            setOriginCamera(
-                Camera(
-                    locationPoint = initialLocation,
-                    pitch = 90.0,
-                    roll = 0.0,
-                    heading = initialHeading
-                )
+        TransformationMatrixCameraController()
+    }
+
+    LaunchedEffect(initialLocation, initialHeading) {
+        Log.d("Blah", "Launched effect")
+        cameraController.setOriginCamera(
+            Camera(
+                locationPoint = initialLocation,
+                pitch = 90.0,
+                roll = 0.0,
+                heading = initialHeading
             )
-            setTranslationFactor(translationFactor)
-        }
+        )
+        arSessionWrapper.resetSession(planeFindingMode = Config.PlaneFindingMode.DISABLED)
+    }
+
+    LaunchedEffect(translationFactor) {
+        cameraController.setTranslationFactor(translationFactor)
     }
 
     Box(modifier = Modifier) {
         ArCameraFeed(
             session = arSessionWrapper,
             onFrame = { frame, displayRotation, session ->
-                cameraController.transformationMatrix =
-                    frame.camera.displayOrientedPose.transformationMatrix
+                if (frame.camera.trackingState == TrackingState.TRACKING) {
+                    cameraController.transformationMatrix =
+                        frame.camera.displayOrientedPose.transformationMatrix
+                }
                 flyoverSceneViewProxy.sceneViewProxy.renderFrame()
             },
             onTapWithHitResult = {},
@@ -127,10 +189,38 @@ public fun FlyoverSceneView(
         arcGISScene = arcGISScene,
         sceneViewProxy = flyoverSceneViewProxy.sceneViewProxy,
         cameraController = cameraController,
-        atmosphereEffect = AtmosphereEffect.Realistic,
-        spaceEffect = SpaceEffect.Stars,
-        modifier = modifier
+        atmosphereEffect = atmosphereEffect,
+        spaceEffect = spaceEffect,
+        modifier = modifier,
+        onViewpointChangedForCenterAndScale = onViewpointChangedForCenterAndScale,
+        onViewpointChangedForBoundingGeometry = onViewpointChangedForBoundingGeometry,
+        graphicsOverlays = graphicsOverlays,
+        sceneViewInteractionOptions = interactionOptions,
+        viewLabelProperties = viewLabelProperties,
+        selectionProperties = selectionProperties,
+        isAttributionBarVisible = isAttributionBarVisible,
+        onAttributionTextChanged = onAttributionTextChanged,
+        onAttributionBarLayoutChanged = onAttributionBarLayoutChanged,
+        analysisOverlays = analysisOverlays,
+        imageOverlays = imageOverlays,
+        timeExtent = timeExtent,
+        onTimeExtentChanged = onTimeExtentChanged,
+        sunTime = sunTime,
+        sunLighting = sunLighting,
+        ambientLightColor = ambientLightColor,
+        onNavigationChanged = onNavigationChanged,
+        onSpatialReferenceChanged = onSpatialReferenceChanged,
+        onLayerViewStateChanged = onLayerViewStateChanged,
+        onInteractingChanged = onInteractingChanged,
+        onCurrentViewpointCameraChanged = onCurrentViewpointCameraChanged,
+        onRotate = onRotate,
+        onScale = onScale,
+        onUp = onUp,
+        onDown = onDown,
+        onSingleTapConfirmed = onSingleTapConfirmed,
+        onDoubleTap = onDoubleTap,
+        onLongPress = onLongPress,
+        onTwoPointerTap = onTwoPointerTap,
+        onPan = onPan
     )
-
-    //sceneViewProxy.setManualRenderingEnabled(true)
 }
