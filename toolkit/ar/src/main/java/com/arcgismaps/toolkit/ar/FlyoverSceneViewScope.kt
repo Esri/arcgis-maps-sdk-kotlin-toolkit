3bb44b36ab3e6c10ccb668d9a06e76fa8eee5b3e
Colin Anderson <colin_anderson@esri.com>
2025-06-13 16:18:55 +0100

    Fix doc

--- toolkit/ar/src/main/java/com/arcgismaps/toolkit/ar/FlyoverSceneViewScope.kt
+++ toolkit/ar/src/main/java/com/arcgismaps/toolkit/ar/FlyoverSceneViewScope.kt
@@ -87,7 +87,7 @@ public class FlyoverSceneViewScope internal constructor(private val sceneViewSco
      * contains the [content] lambda provided by the application. A thin border line is drawn around
      * the entire Callout.
      *
-     * If the given geoelement is a DynamicEntity then the Callout automatically updates its location
+     * If the given [GeoElement] is a DynamicEntity then the Callout automatically updates its location
      * everytime the DynamicEntity changes. The content of the Callout however will not be automatically
      * updated.
      *
