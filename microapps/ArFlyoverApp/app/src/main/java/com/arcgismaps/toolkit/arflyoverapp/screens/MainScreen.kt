4a0e184fcbde4abccc1c6c9a517d46cddff41ac4
Colin Anderson <colin_anderson@esri.com>
2025-06-20 17:01:46 +0100

    Clean up imports

--- microapps/ArFlyoverApp/app/src/main/java/com/arcgismaps/toolkit/arflyoverapp/screens/MainScreen.kt
+++ microapps/ArFlyoverApp/app/src/main/java/com/arcgismaps/toolkit/arflyoverapp/screens/MainScreen.kt
@@ -19,7 +19,6 @@
 package com.arcgismaps.toolkit.arflyoverapp.screens
 
 import android.content.Context
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -40,7 +39,6 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -68,7 +66,6 @@ import com.arcgismaps.toolkit.ar.FlyoverSceneView
 import com.arcgismaps.toolkit.ar.FlyoverSceneViewStatus
 import com.arcgismaps.toolkit.ar.rememberFlyoverSceneViewProxy
 import com.arcgismaps.toolkit.arflyoverapp.R
-import kotlinx.coroutines.launch
 
 private const val KEY_PREF_ACCEPTED_PRIVACY_INFO = "ACCEPTED_PRIVACY_INFO"
 
@@ -125,8 +122,6 @@ fun MainScreen() {
             val initializationStatus: MutableState<FlyoverSceneViewStatus> = remember {
                 mutableStateOf(FlyoverSceneViewStatus.Initializing)
             }
-
-            val coroutineScope = rememberCoroutineScope()
 
             FlyoverSceneView(
                 arcGISScene = arcGISScene,
