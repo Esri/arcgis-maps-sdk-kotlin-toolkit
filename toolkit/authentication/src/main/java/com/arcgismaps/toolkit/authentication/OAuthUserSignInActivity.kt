4aea60a6833653f56eb6c1da6870fc88000d4fbf
Erick <Erick_Solis@esri.com>
2025-06-23 13:59:46 -0700

    update api file

--- toolkit/authentication/src/main/java/com/arcgismaps/toolkit/authentication/OAuthUserSignInActivity.kt
+++ toolkit/authentication/src/main/java/com/arcgismaps/toolkit/authentication/OAuthUserSignInActivity.kt
@@ -185,30 +185,4 @@ public class OAuthUserSignInActivity : ComponentActivity() {
             }
         }
     }
-
-    /**
-     * An ActivityResultContract that takes a String as input and returns a nullable String as output.
-     * The input string represents an IAP authorize URL, and the output string represents a redirect URI as
-     * the result of an IAP sign in prompt, or null if the IAP sign in failed. This contract can be used to launch the
-     * [OAuthUserSignInActivity] for a result.
-     * See [Getting a result from an activity](https://developer.android.com/training/basics/intents/result)
-     * for more details.
-     *
-     * @since 200.8.0
-     */
-    public class IapSignInContract : ActivityResultContract<String, String?>() {
-        override fun createIntent(context: Context, input: String): Intent =
-            Intent(context, OAuthUserSignInActivity::class.java).apply {
-                putExtra(KEY_INTENT_EXTRA_AUTHORIZE_URL, input)
-                putExtra(KEY_INTENT_EXTRA_PROMPT_SIGN_IN, true)
-            }
-
-        override fun parseResult(resultCode: Int, intent: Intent?): String? {
-            return if (resultCode == RESULT_CODE_SUCCESS) {
-                intent?.getStringExtra(KEY_INTENT_EXTRA_OAUTH_RESPONSE_URL)
-            } else {
-                null
-            }
-        }
-    }
 }
