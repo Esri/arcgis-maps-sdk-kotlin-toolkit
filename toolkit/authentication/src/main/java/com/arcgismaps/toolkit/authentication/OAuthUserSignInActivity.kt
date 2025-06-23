b0e668ed324612b8192e8cdfbe92f906e1702180
Erick <Erick_Solis@esri.com>
2025-06-23 11:06:32 -0700

    formatting

--- toolkit/authentication/src/main/java/com/arcgismaps/toolkit/authentication/OAuthUserSignInActivity.kt
+++ toolkit/authentication/src/main/java/com/arcgismaps/toolkit/authentication/OAuthUserSignInActivity.kt
@@ -96,8 +96,7 @@ private const val RESULT_CODE_CANCELED = 2
 @Deprecated(
     message = "This class is deprecated and will be removed in a future release. " +
             "If Identity Aware Proxy (IAP) support is needed, use `AuthenticationActivity` instead. ",
-    replaceWith = ReplaceWith(
-        expression = "com.arcgismaps.toolkit.authentication.AuthenticationActivity"),
+    replaceWith = ReplaceWith(expression = "com.arcgismaps.toolkit.authentication.AuthenticationActivity"),
     level = DeprecationLevel.WARNING
 )
 public class OAuthUserSignInActivity : ComponentActivity() {
