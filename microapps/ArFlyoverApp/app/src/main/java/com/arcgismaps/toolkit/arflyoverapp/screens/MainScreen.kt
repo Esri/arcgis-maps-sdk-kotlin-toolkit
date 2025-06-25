f100dabbfd9a1099d88c496ba4b674aef6417538
Colin Anderson <colin_anderson@esri.com>
2025-06-25 14:03:45 +0100

    Remove unused import

--- microapps/ArFlyoverApp/app/src/main/java/com/arcgismaps/toolkit/arflyoverapp/screens/MainScreen.kt
+++ microapps/ArFlyoverApp/app/src/main/java/com/arcgismaps/toolkit/arflyoverapp/screens/MainScreen.kt
@@ -35,7 +35,6 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
