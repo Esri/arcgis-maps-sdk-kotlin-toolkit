7585950d32a9bbd5dcc88e1f31e90940e893670b
Kaushik Meesala <kmeesala@esri.com>
2025-07-01 15:14:21 -0700

    remove association info icon

--- toolkit/featureforms/src/main/java/com/arcgismaps/toolkit/featureforms/internal/components/utilitynetwork/UtilityAssociations.kt
+++ toolkit/featureforms/src/main/java/com/arcgismaps/toolkit/featureforms/internal/components/utilitynetwork/UtilityAssociations.kt
@@ -20,20 +20,15 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material3.DividerDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
@@ -264,13 +259,6 @@ private fun AssociationItem(
                         maxLines = 1
                     )
                 }
-            }
-            Spacer(modifier = Modifier.width(16.dp))
-            IconButton(
-                onClick = onDetailsClick,
-                modifier = Modifier
-            ) {
-                Icon(Icons.Outlined.Info, contentDescription = "association details")
             }
         }
     }
