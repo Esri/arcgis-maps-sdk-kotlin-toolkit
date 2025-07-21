927766828653a0d997f77e5bf77b8295736e6632
Hudson Miears <hud10837@esri.com>
2025-07-21 17:54:26 +0300

    use TimeInput instead of TimePicker

--- microapps/SceneViewLightingOptionsApp/app/src/main/java/com/arcgismaps/toolkit/sceneviewlightingoptionsapp/screens/MainScreen.kt
+++ microapps/SceneViewLightingOptionsApp/app/src/main/java/com/arcgismaps/toolkit/sceneviewlightingoptionsapp/screens/MainScreen.kt
@@ -32,7 +32,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.TimePicker
+import androidx.compose.material3.TimeInput
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
@@ -279,7 +279,7 @@ fun SunTimeOptions(
             }
         },
         text = {
-            TimePicker(state = timePickerState)
+            TimeInput(state = timePickerState)
         }
     )
 }
