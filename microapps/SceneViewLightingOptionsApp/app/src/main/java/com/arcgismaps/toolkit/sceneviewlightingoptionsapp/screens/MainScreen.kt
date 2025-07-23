4aaa169ccbe209c91de2b9cca4116aca62be7601
Hudson Miears <hud10837@esri.com>
2025-07-23 09:59:20 +0300

    use decorFitsSystemWindows = false

--- microapps/SceneViewLightingOptionsApp/app/src/main/java/com/arcgismaps/toolkit/sceneviewlightingoptionsapp/screens/MainScreen.kt
+++ microapps/SceneViewLightingOptionsApp/app/src/main/java/com/arcgismaps/toolkit/sceneviewlightingoptionsapp/screens/MainScreen.kt
@@ -18,9 +18,16 @@
 
 package com.arcgismaps.toolkit.sceneviewlightingoptionsapp.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.AlertDialog
@@ -29,10 +36,12 @@ import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.TimeInput
+import androidx.compose.material3.TimePicker
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
@@ -45,6 +54,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import com.arcgismaps.geometry.Point
 import com.arcgismaps.geometry.SpatialReference
 import com.arcgismaps.mapping.ArcGISScene
@@ -263,25 +275,60 @@ fun SunTimeOptions(
         initialMinute = currentTime.minute,
         is24Hour = true
     )
-    AlertDialog(
+    Dialog(
         onDismissRequest = onDismissRequest,
-        confirmButton = {
-            TextButton(onClick = {
-                onSetSunTime(
-                    OffsetDateTime.now(ZoneId.of("UTC"))
-                        .withHour(timePickerState.hour)
-                        .withMinute(timePickerState.minute)
-                        .toInstant()
+        properties = DialogProperties(
+            usePlatformDefaultWidth = false,
+            decorFitsSystemWindows = false
+        ),
+    ) {
+        Surface(
+            shape = MaterialTheme.shapes.extraLarge,
+            tonalElevation = 6.dp,
+            modifier = Modifier
+                .width(IntrinsicSize.Min)
+                .height(IntrinsicSize.Min)
+                .background(
+                    shape = MaterialTheme.shapes.extraLarge,
+                    color = MaterialTheme.colorScheme.surface
+                ),
+        ) {
+            Column(
+                modifier = Modifier.padding(24.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 20.dp),
+                    text = "Set Sun Time",
+                    style = MaterialTheme.typography.labelMedium
                 )
-                onDismissRequest()
-            }) {
-                Text("Confirm")
+                TimePicker(state = timePickerState)
+                Row(
+                    modifier = Modifier
+                        .height(40.dp)
+                        .fillMaxWidth()
+                ) {
+                    Spacer(modifier = Modifier.weight(1f))
+                    TextButton(
+                        onClick = onDismissRequest
+                    ) { Text("Cancel") }
+                    TextButton(
+                        onClick = {
+                            onSetSunTime(
+                                OffsetDateTime.now(ZoneId.of("UTC"))
+                                    .withHour(timePickerState.hour)
+                                    .withMinute(timePickerState.minute)
+                                    .toInstant()
+                            )
+                            onDismissRequest()
+                        }
+                    ) { Text("OK") }
+                }
             }
-        },
-        text = {
-            TimeInput(state = timePickerState)
         }
-    )
+    }
 }
 
 /**
@@ -451,3 +498,4 @@ data class LightingOptionsState(
     val atmosphereEffect: MutableState<AtmosphereEffect>,
     val spaceEffect: MutableState<SpaceEffect>
 )
+
