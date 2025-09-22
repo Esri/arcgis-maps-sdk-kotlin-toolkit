76ec0334a19f2677804dac8b19a56042fca8eaa4
Puneet Prakash <puneet_prakash@esri.com>
2025-09-22 14:48:08 -0700

    Add fix for navigation not working Change bottomsheet behavior for onDismiss

--- microapps/PopupApp/app/src/main/java/com/arcgismaps/toolkit/popupapp/screens/mapscreen/MainScreen.kt
+++ microapps/PopupApp/app/src/main/java/com/arcgismaps/toolkit/popupapp/screens/mapscreen/MainScreen.kt
@@ -22,14 +22,18 @@ package com.arcgismaps.toolkit.popupapp.screens.mapscreen
 
 import android.widget.Toast
 import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.SheetValue
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.material3.rememberStandardBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -70,11 +74,17 @@ fun MainScreen(viewModel: MapViewModel = viewModel()) {
                 Popup(
                     viewModel.popupState!!,
                     Modifier.animateContentSize(),
-                    onDismiss = { scope.launch { scaffoldState.bottomSheetState.partialExpand() } }
+                    onDismiss = { scope.launch {
+                        viewModel.updatePopupState(null)
+                        unselectFeature(viewModel.geoElement, viewModel.layer)
+                        scaffoldState.bottomSheetState.hide()
+                    } }
                 )
             }
         },
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(WindowInsets.safeDrawing.asPaddingValues()),
         scaffoldState = scaffoldState,
         sheetSwipeEnabled = true,
         topBar = null
