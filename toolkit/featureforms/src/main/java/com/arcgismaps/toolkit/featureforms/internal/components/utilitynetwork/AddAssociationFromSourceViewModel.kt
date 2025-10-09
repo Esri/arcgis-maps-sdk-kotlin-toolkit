177db63ac7dcb972c5221148d6311db1a5668950
Kaushik Meesala <kmeesala@esri.com>
2025-10-09 11:30:49 -0700

    update doc

--- toolkit/featureforms/src/main/java/com/arcgismaps/toolkit/featureforms/internal/components/utilitynetwork/AddAssociationFromSourceViewModel.kt
+++ toolkit/featureforms/src/main/java/com/arcgismaps/toolkit/featureforms/internal/components/utilitynetwork/AddAssociationFromSourceViewModel.kt
@@ -195,8 +195,7 @@ internal class AddAssociationFromSourceViewModel(
             }.distinctUntilChanged().collect { pair ->
                 val source = pair.first
                 val assetType = pair.second
-                // If no source, asset type is selected or the asset type is not valid for the source,
-                // clear the candidates and return an error state
+                // If no source, asset type is selected return an error state
                 if (source == null || assetType == null) {
                     _featureCandidatesUiState.value = FeatureCandidatesUiState(
                         isLoading = false,
