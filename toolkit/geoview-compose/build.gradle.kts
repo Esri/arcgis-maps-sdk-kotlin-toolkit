547495b554dfa9b611a0cfd03bbc5d484bc84ced
Roman Savitski <rsavitski@esri.com>
2026-03-19 16:53:32 +0000

    Update toolkit/geoview-compose/build.gradle.kts
    Co-authored-by: Shubham Sharma <shubhamsharma@esri.com>

--- toolkit/geoview-compose/build.gradle.kts
+++ toolkit/geoview-compose/build.gradle.kts
@@ -21,7 +21,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
     id("artifact-deploy")
-    id("android-integration-testing")
+    alias(libs.plugins.android.integration.testing)
     alias(libs.plugins.binary.compatibility.validator) apply true
 }
 
