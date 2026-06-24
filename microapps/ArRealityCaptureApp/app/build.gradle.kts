eb7cb990bec4ed1c94302f6364ba0c3595290101
Gunther Heppner <gheppner@esri.com>
2026-06-24 18:30:57 +0200

    fix AGP 9 compile error

--- microapps/ArRealityCaptureApp/app/build.gradle.kts
+++ microapps/ArRealityCaptureApp/app/build.gradle.kts
@@ -18,7 +18,6 @@
 
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
