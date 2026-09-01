891192d19437b304c90cf5e810c6f0bf496422c3
Colin Anderson <colin_anderson@esri.com>
2026-09-01 13:43:53 +0100

    Show fades to indicate there is scrollable content

--- toolkit/buildingexplorer/src/main/java/com/arcgismaps/toolkit/buildingexplorer/BuildingExplorer.kt
+++ toolkit/buildingexplorer/src/main/java/com/arcgismaps/toolkit/buildingexplorer/BuildingExplorer.kt
@@ -18,13 +18,17 @@
 
 package com.arcgismaps.toolkit.buildingexplorer
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.ArrowDropUp
@@ -34,6 +38,7 @@ import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
@@ -46,6 +51,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.arcgismaps.mapping.layers.buildingscene.BuildingGroupSublayer
@@ -63,7 +70,10 @@ public fun BuildingExplorer(
     modifier: Modifier = Modifier
 ) {
     Box(modifier = modifier) {
+        val listState = rememberLazyListState()
+
         LazyColumn(
+            state = listState,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             item {
@@ -123,6 +133,33 @@ public fun BuildingExplorer(
             item {
                 BuildingExplorer(buildingSceneLayerState = state.buildingSceneLayerState)
             }
+        }
+        // show a fade to indicate that there is scrollable content
+        if (listState.canScrollBackward) {
+            Box(
+                modifier = Modifier
+                    .align(Alignment.TopCenter)
+                    .fillMaxWidth()
+                    .height(50.dp)
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(MaterialTheme.colorScheme.surface, Color.Transparent)
+                        )
+                    )
+            )
+        }
+        if (listState.canScrollForward) {
+            Box(
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .fillMaxWidth()
+                    .height(50.dp)
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(Color.Transparent, MaterialTheme.colorScheme.surface)
+                        )
+                    )
+            )
         }
     }
 }
